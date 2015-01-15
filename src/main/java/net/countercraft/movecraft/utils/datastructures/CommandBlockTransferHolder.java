311efe69de8d64630c7d3c864b4248001ddce33a
msummers123 <msummers@slcval.com>
2015-01-15 07:28:33 -0700

    Cannon Tracers - TracerRateTicks in config.yml higher number = more space between tracers
    FireballLifespan in config.yml - Time in seconds before fireballs
    launched from dispensers delete themselves
    
    
    HalfSpeedUnderwater: true in .craft files - If true, then the craft
    moves at half speed underwater. Affects all forms of movement.
    
    
    VertCruiseSkipblocks in .craft files - like cruiseSkipBlocks, but only
    for vertical movement (via ascend and descend signs)
    
    
    MaxHeightAboveGround in .craft files - if a .craft is more than this
    above the ground, it will descend as it moves
    
    
    New permission: movecraft.commands Allows access to /cruise, /pilot, and
    /release commands. Does not affect /manoverboard or /cruiseoff. Also
    allows the right to place cruise signs.
    
    
    Also, explosions will no longer cause water spillage. And I have taken
    another stab at fixing the "overallsinkpercent" system to prevent crafts
    spontaneously sinking. If anyone has continued problems please let me
    know.

---
+++ src/main/java/net/countercraft/movecraft/utils/datastructures/CommandBlockTransferHolder.java
@@ -0,0 +1,37 @@
+/*
+ * This file is part of Movecraft.
+ *
+ *     Movecraft is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Movecraft is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Movecraft.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.countercraft.movecraft.utils.datastructures;
+
+public class CommandBlockTransferHolder extends TransferData {
+	private final String commandText;
+	private final String commandName;
+
+	public CommandBlockTransferHolder( byte data, String commandText, String commandName ) {
+		super( data );
+		this.commandText = commandText;
+		this.commandName = commandName;
+	}
+
+	public String getText() {
+		return this.commandText;
+	}
+
+	public String getName() {
+		return this.commandName;
+	}
+}
