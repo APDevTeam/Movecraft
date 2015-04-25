7c04dfedb4cff855350bfc533a2d85c1c257a759
mwkaicz <mwkaicz@gmail.com>
2015-04-25 02:47:04 +0200

    Fix: - subcraft prevention - WG flag definition (now it's defined from class name via constructor newInstance) - Towny explosion prevention - copy translations

---
+++ src/main/java/net/countercraft/movecraft/utils/WGCustomFlagsUtils.java
@@ -0,0 +1,101 @@
+package net.countercraft.movecraft.utils;
+
+import com.mewin.WGCustomFlags.WGCustomFlagsPlugin;
+import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import net.countercraft.movecraft.Movecraft;
+import org.bukkit.Location;
+
+/**
+ *
+ * @author mwkaicz <mwkaicz@gmail.com>
+ */
+public class WGCustomFlagsUtils {
+    
+    
+    public StateFlag getNewStateFlag(String name, boolean def){
+        Constructor<?> cc = getStateFlagConstructor();
+        if (cc == null){return null;}
+        Object o;
+        try {
+            o = cc.newInstance(name, def);
+            return (StateFlag) o;
+        } catch(InstantiationException ex){
+            return null; 
+        } catch(IllegalAccessException  ex){
+            return null; 
+        } catch(IllegalArgumentException ex){
+            return null; 
+        } catch(InvocationTargetException ex) {
+            return null; 
+        }
+    }
+     
+    public Constructor<?> getStateFlagConstructor(){
+        try {
+            Class<?> c = Class.forName("com.sk89q.worldguard.protection.flags.StateFlag");
+            Constructor<?> cc = c.getConstructor(String.class,boolean.class);
+            return cc;
+        } catch (ClassNotFoundException ex){
+            return null; 
+        } catch (NoSuchMethodException ex){
+            return null; 
+        } catch (SecurityException ex) {
+            return null;
+        }
+    }
+    
+    public boolean registerStageFlag(Object o){
+        WGCustomFlagsPlugin wgcf = Movecraft.getInstance().getWGCustomFlagsPlugin();
+        if (wgcf != null){
+            Constructor<?> cc = getStateFlagConstructor();
+            if (cc == null){return false;}
+            try {
+                wgcf.addCustomFlag((Flag) o);   
+            } catch (Exception e){
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+    
+    
+    public void init(){
+        if (Movecraft.FLAG_PILOT != null){
+            this.registerStageFlag(Movecraft.FLAG_PILOT);
+        }
+        if (Movecraft.FLAG_MOVE != null){
+            this.registerStageFlag(Movecraft.FLAG_MOVE);
+        }
+        if (Movecraft.FLAG_ROTATE != null){
+            this.registerStageFlag(Movecraft.FLAG_ROTATE);
+        }
+        if (Movecraft.FLAG_SINK != null){
+            this.registerStageFlag(Movecraft.FLAG_SINK);
+        }   
+    } 
+    
+    public boolean validateFlag(Location loc, Object flag){
+        if (flag!=null){
+            StateFlag.State state = (StateFlag.State)Movecraft.getInstance().getWorldGuardPlugin().getRegionManager(loc.getWorld()).getApplicableRegions(loc).getFlag((Flag)flag);
+            return state != null && state == StateFlag.State.ALLOW;
+        }else{
+            return true;
+        }
+    }
+ 
+    public boolean validateFlag(Location loc, Object flag, LocalPlayer lp){
+        if (flag!=null){
+            StateFlag.State state = (StateFlag.State)Movecraft.getInstance().getWorldGuardPlugin().getRegionManager(loc.getWorld()).getApplicableRegions(loc).getFlag((Flag)flag,lp);
+            return state != null && state == StateFlag.State.ALLOW;
+        }else{
+            return true;
+        }
+    }
+    
+    
+}
