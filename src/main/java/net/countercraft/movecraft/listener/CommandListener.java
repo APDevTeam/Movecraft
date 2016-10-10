59449f7afd97014726d8c8afcf8d85d278ea6a1e
Germán L. Osella Massa <german.osella@nexo.unnoba.edu.ar>
2016-10-09 21:15:29 -0300

    /manoverboard teleport location was a little off

--- src/main/java/net/countercraft/movecraft/listener/CommandListener.java
+++ src/main/java/net/countercraft/movecraft/listener/CommandListener.java
@@ -110,9 +110,9 @@ public class CommandListener implements CommandExecutor {
 
 			}
 		}
-		int telX=craft.getMinX()+(maxDX / 2);
-		int telZ=craft.getMinZ()+(maxDZ / 2);
-		int telY=maxY;
+		double telX=craft.getMinX()+(maxDX / 2.0);
+		double telZ=craft.getMinZ()+(maxDZ / 2.0);
+		double telY=maxY + 1.0;
 		Location telPoint=new Location(w, telX, telY, telZ);
 		return telPoint;
 	}
