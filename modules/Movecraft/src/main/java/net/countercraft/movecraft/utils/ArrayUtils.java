1ecf23a2b3b89a7f844880fbfc6e7109c266090a
cccm5 <danieldavidson@aol.com>
2017-08-23 10:55:36 -0400

    Fix subtraction

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/utils/ArrayUtils.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/utils/ArrayUtils.java
@@ -1,17 +1,18 @@
 package net.countercraft.movecraft.utils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ArrayUtils {
     public static <T> List<T> subtractAsList(T[] array1, T[] array2){
-        List<T> list = Arrays.asList(array1);
+        List<T> list = new ArrayList<T>(Arrays.asList(array1));
         list.removeAll(Arrays.asList(array2));
         return list;
     }
 
     public static <T> T[] subtract(T[] array1, T[] array2){
-        List<T> list = Arrays.asList(array1);
+        List<T> list = new ArrayList<T>(Arrays.asList(array1));
         list.removeAll(Arrays.asList(array2));
         return list.toArray(array1.clone());
     }
