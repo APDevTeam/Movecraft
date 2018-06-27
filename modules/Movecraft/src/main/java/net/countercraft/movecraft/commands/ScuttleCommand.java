1f985a05c9e9870d222bb7be08c8845355ddeb53
cccm5 <danieldavidson@aol.com>
2018-06-26 20:55:22 -0400

    Minor fixes to ScuttleCommand

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/commands/ScuttleCommand.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/commands/ScuttleCommand.java
@@ -1,5 +1,6 @@
 package net.countercraft.movecraft.commands;
 
+import net.countercraft.movecraft.craft.Craft;
 import net.countercraft.movecraft.craft.CraftManager;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -10,50 +11,45 @@ import org.bukkit.entity.Player;
 public class ScuttleCommand implements CommandExecutor {
 
 
-	@Override
-	public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
 
-		if(!command.getName().equalsIgnoreCase("scuttle")){
-			return false;
-		}
-		if(!(commandSender instanceof Player)){
-			commandSender.sendMessage("You must be a player to scuttle a craft");
-			return true;
-		}
+        if (!command.getName().equalsIgnoreCase("scuttle")) {
+            return false;
+        }
 
-		// Scuttle self
-		if(commandSender.hasPermission("movecraft.commands.scuttle.self") && strings.length == 0)
-		{
-			if(CraftManager.getInstance().getCraftByPlayer(Bukkit.getPlayer(commandSender.getName())) == null)
-			{
-				commandSender.sendMessage("You must be piloting a craft to perform this command");
-				return true;
-			}
-			CraftManager.getInstance().getCraftByPlayer(Bukkit.getPlayer(commandSender.getName())).setSinking(true);
-		}
+        // Scuttle other player
+        if (commandSender.hasPermission("movecraft.commands.scuttle.others") && strings.length >= 1) {
+            Player player = Bukkit.getPlayer(strings[0]);
 
-		// Scuttle other player
-		if(commandSender.hasPermission("movecraft.commands.scuttle.others") && strings.length == 1)
-		{
-			Player player = Bukkit.getPlayer(strings[0]);
+            if (player == null) {
+                commandSender.sendMessage("Player supplied must be online");
+                return true;
+            }
 
-			if(player == null)
-			{
-				commandSender.sendMessage("Player supplied must be online");
-				return true;
-			}
+            if (CraftManager.getInstance().getCraftByPlayer(player) == null) {
+                commandSender.sendMessage("Player supplied must be piloting a craft to perform this command");
+                return true;
+            }
 
-			if(CraftManager.getInstance().getCraftByPlayer(player) == null)
-			{
-				commandSender.sendMessage("Player supplied must be piloting a craft to perform this command");
-				return true;
-			}
+            CraftManager.getInstance().getCraftByPlayer(player).setSinking(true);
+        }
 
-			CraftManager.getInstance().getCraftByPlayer(player).setSinking(true);
-		}
+        // Scuttle self
+        if (commandSender.hasPermission("movecraft.commands.scuttle.self") && strings.length == 0) {
+            if (!(commandSender instanceof Player)) {
+                commandSender.sendMessage("You must be a player to scuttle a craft");
+                return true;
+            }
+            Craft craft = CraftManager.getInstance().getCraftByPlayer(Bukkit.getPlayer(commandSender.getName()));
+            if (craft == null) {
+                commandSender.sendMessage("You must be piloting a craft to perform this command");
+                return true;
+            }
+            craft.setSinking(true);
+        }
+        commandSender.sendMessage("Scuttle was activated. Abandon ship!");
+        return true;
 
-		commandSender.sendMessage("Scuttle was activated. Abandon ship!");
-		return true;
-
-	}
+    }
 }
