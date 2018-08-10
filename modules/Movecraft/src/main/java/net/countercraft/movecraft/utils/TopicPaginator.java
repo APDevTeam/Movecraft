4918bbece32d7cd731ea068a5144b93f00360a4e
cccm5 <danieldavidson@aol.com>
2018-08-09 20:29:20 -0400

    Add missing space in page title to paginator

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/utils/TopicPaginator.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/utils/TopicPaginator.java
@@ -29,7 +29,7 @@ public class TopicPaginator {
         if(!isInBounds(pageNumber))
             throw new IndexOutOfBoundsException("Page number " + pageNumber + " exceeds bounds <" + 1 + "," + getPageCount() + ">");
         String[] tempLines = new String[pageNumber == getPageCount() ? (lines.size()%CLOSED_CHAT_PAGE_HEIGHT) + 1 : CLOSED_CHAT_PAGE_HEIGHT];
-        tempLines[0] = "§e§l--- §r§6" + title +"§e§l-- §r§6page §c" + pageNumber + "§e/§c" + getPageCount() + " §e§l---";
+        tempLines[0] = "§e§l--- §r§6" + title +" §e§l-- §r§6page §c" + pageNumber + "§e/§c" + getPageCount() + " §e§l---";
         for(int i = 0; i< tempLines.length-1; i++)
             tempLines[i+1] = lines.get(((CLOSED_CHAT_PAGE_HEIGHT-1) * (pageNumber-1)) + i);
         return tempLines;
