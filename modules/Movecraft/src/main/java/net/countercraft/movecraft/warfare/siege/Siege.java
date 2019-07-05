de67f0b71528d779d79138036ad8c35cc824db47
TylerS1066 <TylerS1066@users.noreply.github.com>
2019-07-05 09:51:40 -0500

    Oops

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/warfare/siege/Siege.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/warfare/siege/Siege.java
@@ -8,7 +8,7 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class Siege {
     @NotNull private final List<Integer> daysOfWeek;
-    private final List<String> craftsToWin, commandsOnStart, commandsOnLose, commandsOnWin;
+    @NotNull private final List<String> craftsToWin, commandsOnStart, commandsOnLose, commandsOnWin;
     private final int scheduleStart, scheduleEnd, delayBeforeStart, duration, dailyIncome, cost;
     @NotNull private final String attackRegion, captureRegion, name;
     @NotNull private final AtomicReference<SiegeStage> stage;
