5cb4026f210818f563d8dc2a80fb0d8131ffc8b6
eirikh1996 <eirikhetland1996@gmail.com>
2019-07-14 23:24:48 +0200

    Reverted localisation of Region damaged sign

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/sign/RegionDamagedSign.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/sign/RegionDamagedSign.java
@@ -25,7 +25,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import java.util.UUID;
 
 public class RegionDamagedSign implements Listener {
-    private final String HEADER = ChatColor.RED + I18nSupport.getInternationalisedString("Region Damaged");
+    private final String HEADER = ChatColor.RED + "REGION DAMAGED!";
 
     @EventHandler
     public void onSignRightClick(PlayerInteractEvent event){
