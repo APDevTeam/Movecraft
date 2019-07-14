45248c42c1676160e73936f56d27417dec098a87
cccm5 <daniel.h.davidson@uconn.edu>
2019-07-13 21:00:26 -0400

    Fix assault formatting

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/commands/AssaultInfoCommand.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/commands/AssaultInfoCommand.java
@@ -17,6 +17,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static net.countercraft.movecraft.utils.ChatUtils.MOVECRAFT_COMMAND_PREFIX;
@@ -80,35 +82,40 @@ public class AssaultInfoCommand implements CommandExecutor {
         }
 
         boolean canBeAssaulted = true;
-        String output = I18nSupport.getInternationalisedString("AssaultInfo - Name") + " ";
+        List<String> lines = new ArrayList<>();
+        String output = I18nSupport.getInternationalisedString("AssaultInfo - Name") + ": ";
         output += assaultRegion.getId();
-        output += ", "+I18nSupport.getInternationalisedString("AssaultInfo - Owner")+" " ;
+        lines.add(output);
+        output = I18nSupport.getInternationalisedString("AssaultInfo - Owner")+": " ;
         output += WorldguardUtils.getRegionOwnerList(assaultRegion);
-        output += ", "+I18nSupport.getInternationalisedString("AssaultInfo - Cap")+" ";
+        lines.add(output);
+        output = I18nSupport.getInternationalisedString("AssaultInfo - Cap")+": ";
         double maxDamage = AssaultUtils.getMaxDamages(assaultRegion);
         output += String.format("%.2f", maxDamage);
-        output += ", "+I18nSupport.getInternationalisedString("AssaultInfo - Cost")+" ";
+        lines.add(output);
+        output = I18nSupport.getInternationalisedString("AssaultInfo - Cost")+": ";
         double cost = AssaultUtils.getCostToAssault(assaultRegion);
         output += String.format("%.2f", cost);
+        lines.add(output);
         for (Assault assault : Movecraft.getInstance().getAssaultManager().getAssaults()) {
             if (assault.getRegionName().equals(assaultRegion.getId()) && System.currentTimeMillis() - assault.getStartTime() < Settings.AssaultCooldownHours * (60 * 60 * 1000)) {
                 canBeAssaulted = false;
-                output += ", "+I18nSupport.getInternationalisedString("AssaultInfo - Not Assaultable Damaged");
+                lines.add("- "+I18nSupport.getInternationalisedString("AssaultInfo - Not Assaultable Damaged"));
                 break;
             }
         }
         if (!AssaultUtils.areDefendersOnline(assaultRegion)) {
             canBeAssaulted = false;
-            output += ", "+I18nSupport.getInternationalisedString("AssaultInfo - Not Assaultable Defenders");
+            lines.add("- "+I18nSupport.getInternationalisedString("AssaultInfo - Not Assaultable - Insufficient Defenders"));
         }
         if (assaultRegion.isMember(lp)) {
-            output += ", "+I18nSupport.getInternationalisedString("AssaultInfo - Not Assaultable Member");
+            lines.add("- "+I18nSupport.getInternationalisedString("AssaultInfo - Not Assaultable - You Are Member"));
             canBeAssaulted = false;
         }
         if (canBeAssaulted) {
-            output += ", "+I18nSupport.getInternationalisedString("AssaultInfo - Assaultable");
+            lines.add("- "+I18nSupport.getInternationalisedString("AssaultInfo - Assaultable"));
         }
-        player.sendMessage(output);
+        player.sendMessage(lines.toArray(new String[1]));
         return true;
     }
 }
