1b1d7e38cc70309fdf5e3a3b66650d6cea2b39e9
cccm5 <danieldavidson@aol.com>
2019-10-05 16:49:43 -0400

    reset fall damage on manoverboard

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/commands/ManOverboardCommand.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/commands/ManOverboardCommand.java
@@ -11,6 +11,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 import static net.countercraft.movecraft.utils.ChatUtils.MOVECRAFT_COMMAND_PREFIX;
 
@@ -55,6 +56,7 @@ public class ManOverboardCommand implements CommandExecutor{
             return true;
         }
 
+        player.setVelocity(new Vector(0, 0, 0));
         player.teleport(telPoint);
         return true;
     }
