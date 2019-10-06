814275c5bf030b2af9e878d3af39b6ad1fb43c9b
eirikh1996 <eirikhetland1996@gmail.com>
2019-10-06 12:57:17 +0200

    Created TownyCompatManager

---
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/towny/TownyCompatManager.java
@@ -0,0 +1,27 @@
+package net.countercraft.movecraft.towny;
+
+import com.palmergames.bukkit.towny.object.Town;
+import com.palmergames.bukkit.towny.object.TownBlock;
+import net.countercraft.movecraft.MovecraftLocation;
+import net.countercraft.movecraft.events.CraftTranslateEvent;
+import net.countercraft.movecraft.utils.TownyUtils;
+import net.countercraft.movecraft.utils.TownyWorldHeightLimits;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class TownyCompatManager implements Listener {
+
+    @EventHandler
+    public void onCraftTranslate(CraftTranslateEvent event){
+
+        TownyWorldHeightLimits whLimits = TownyUtils.getWorldLimits(event.getCraft().getW());
+        TownBlock townBlock = null;
+        for (MovecraftLocation ml : event.getNewHitBox()){
+            townBlock = TownyUtils.getTownBlock(ml.toBukkit(event.getCraft().getW()));
+        }
+        if (townBlock == null){
+            return;
+        }
+        Town town = TownyUtils.getTown(townBlock);
+    }
+}
