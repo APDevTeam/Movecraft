46148aeb499ba5612ba933f95a464cd58ac5ecea
eirikh1996 <eirikhetland1996@gmail.com>
2019-12-17 20:31:39 +0100

    Further simplification

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/async/translation/TranslationTask.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/async/translation/TranslationTask.java
@@ -519,8 +519,8 @@ public class TranslationTask extends AsyncTask {
                 //This has to be subtracted by one, or non-passthrough blocks will be within the y drop path
                 //obstructing the craft
                 Material testType = ml.translate(0, dropDistance - 1, 0).toBukkit(craft.getW()).getBlock().getType();
-                if (testType != Material.AIR && !craft.getType().getPassthroughBlocks().contains(testType)) {
-                    hitGround = true;
+                hitGround = testType != Material.AIR && !craft.getType().getPassthroughBlocks().contains(testType);
+                if (hitGround) {
                     break;
                 }
             }
