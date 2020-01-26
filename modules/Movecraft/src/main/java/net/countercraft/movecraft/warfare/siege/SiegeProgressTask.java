6b5f66e5ef42dd8a50a25fd8ed0fffe422f7dbf9
eirikh1996 <eirikhetland1996@gmail.com>
2020-01-26 19:53:31 +0100

    Fixed sieges not ending as well as a formatting issue (#218)

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/warfare/siege/SiegeProgressTask.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/warfare/siege/SiegeProgressTask.java
@@ -1,20 +1,18 @@
 package net.countercraft.movecraft.warfare.siege;
 
-import com.avaje.ebean.validation.NotNull;
-import java.util.List;
 import com.sk89q.worldguard.domains.DefaultDomain;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import net.countercraft.movecraft.Movecraft;
-import net.countercraft.movecraft.craft.Craft;
 import net.countercraft.movecraft.MovecraftLocation;
-import net.countercraft.movecraft.craft.CraftManager;
 import net.countercraft.movecraft.config.Settings;
+import net.countercraft.movecraft.craft.Craft;
+import net.countercraft.movecraft.craft.CraftManager;
 import net.countercraft.movecraft.localisation.I18nSupport;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.List;
 
 public class SiegeProgressTask extends SiegeTask {
 
@@ -24,13 +22,14 @@ public class SiegeProgressTask extends SiegeTask {
 
     //every 20 ticks = 1 second
     public void run() {
+        Bukkit.broadcastMessage(String.valueOf((siege.getDuration() - ((System.currentTimeMillis() - siege.getStartTime()) / 1000))));
         if ((siege.getDuration() - ((System.currentTimeMillis() - siege.getStartTime()) / 1000)) % Settings.SiegeTaskSeconds != 0) {
             return;
         }
 
         Player siegeLeader = Movecraft.getInstance().getServer().getPlayer(siege.getPlayerUUID());
         Craft siegeCraft = CraftManager.getInstance().getCraftByPlayer(siegeLeader);
-        int timeLeft = (siege.getDuration() - (((int)System.currentTimeMillis() - siege.getStartTime())/1000));
+        int timeLeft = (siege.getDuration() - (int) ((System.currentTimeMillis() - siege.getStartTime())/1000));
 
         if (timeLeft > 10) {
             if (!leaderPilotingShip(siegeCraft)) {
@@ -56,7 +55,7 @@ public class SiegeProgressTask extends SiegeTask {
             endSiege(siegeCraft, siegeLeader);
         }
 
-        siege.setStage(SiegeStage.INACTIVE);
+
 
         for (Player p : Bukkit.getOnlinePlayers()){
             p.playSound(p.getLocation(), Sound.ENTITY_WITHER_DEATH, 1,0);
@@ -77,6 +76,7 @@ public class SiegeProgressTask extends SiegeTask {
         else {
             failSiege(siegeLeader);
         }
+        siege.setStage(SiegeStage.INACTIVE);
     }
 
     private void winSiege(Player siegeLeader) {
