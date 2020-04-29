3b454df163bc688a41d8204f897d2e19a4ff8a66
eirikh1996 <eirikhetland1996@gmail.com>
2020-04-29 03:43:15 +0200

    Fixed IllegalArgumentException due to negative progress in SiegeProgressTask

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/warfare/siege/SiegeProgressTask.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/warfare/siege/SiegeProgressTask.java
@@ -28,7 +28,7 @@ public class SiegeProgressTask extends SiegeTask {
     public void run() {
         addPlayersToProgressBar();
         int timeLeft = (siege.getDuration() - (int) ((System.currentTimeMillis() - siege.getStartTime())/1000));
-        double progress = (double) (siege.getDelayBeforeStart() - timeLeft) / (double) (siege.getDuration() - siege.getDelayBeforeStart());
+        double progress = (double) (siege.getDelayBeforeStart() - timeLeft) / (double) siege.getDuration();
         siege.getProgressBar().setProgress(Math.min(progress, 1.0));
         Player siegeLeader = Movecraft.getInstance().getServer().getPlayer(siege.getPlayerUUID());
         Craft siegeCraft = CraftManager.getInstance().getCraftByPlayer(siegeLeader);
