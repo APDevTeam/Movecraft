0ddd1ac1f2b0364fbb52d571af26441b04022d40
TylerS1066 <TylerS1066@users.noreply.github.com>
2020-07-12 17:49:57 -0500

    Removed scuttle command

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/Movecraft.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/Movecraft.java
@@ -386,7 +386,6 @@ public class Movecraft extends JavaPlugin {
             this.getCommand("craftreport").setExecutor(new CraftReportCommand());
             this.getCommand("manoverboard").setExecutor(new ManOverboardCommand());
             this.getCommand("contacts").setExecutor(new ContactsCommand());
-            this.getCommand("scuttle").setExecutor(new ScuttleCommand());
 
             if(Settings.SiegeEnable)
                 this.getCommand("siege").setExecutor(new SiegeCommand());
