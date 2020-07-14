3929c98a1855304d1d34044f6258f95c0f08e872
TylerS1066 <TylerS1066@users.noreply.github.com>
2020-07-14 18:03:21 -0500

    Revert "Removed scuttle command"
    This reverts commit 0ddd1ac1f2b0364fbb52d571af26441b04022d40.

---
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/commands/ScuttleCommand.java
@@ -0,0 +1,56 @@
+package net.countercraft.movecraft.commands;
+
+import net.countercraft.movecraft.craft.Craft;
+import net.countercraft.movecraft.craft.CraftManager;
+import net.countercraft.movecraft.localisation.I18nSupport;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import static net.countercraft.movecraft.utils.ChatUtils.MOVECRAFT_COMMAND_PREFIX;
+
+public class ScuttleCommand implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+        if (!command.getName().equalsIgnoreCase("scuttle")) {
+            return false;
+        }
+
+        Craft craft = null;
+        // Scuttle other player
+        if (commandSender.hasPermission("movecraft.commands.scuttle.others") && strings.length >= 1) {
+            Player player = Bukkit.getPlayer(strings[0]);
+            if (player == null) {
+                commandSender.sendMessage(MOVECRAFT_COMMAND_PREFIX + I18nSupport.getInternationalisedString("Scuttle - Must Be Online"));
+                return true;
+            }
+            craft = CraftManager.getInstance().getCraftByPlayer(player);
+        } else if (commandSender.hasPermission("movecraft.commands.scuttle.self") && strings.length == 0) {
+            if (!(commandSender instanceof Player)) {
+                commandSender.sendMessage(MOVECRAFT_COMMAND_PREFIX + I18nSupport.getInternationalisedString("Scuttle - Must Be Player"));
+                return true;
+            }
+            craft = CraftManager.getInstance().getCraftByPlayer(Bukkit.getPlayer(commandSender.getName()));
+        }
+        if (craft == null) {
+            commandSender.sendMessage(MOVECRAFT_COMMAND_PREFIX +  I18nSupport.getInternationalisedString("You must be piloting a craft"));
+            return true;
+        }
+        if(craft.getSinking()){
+            commandSender.sendMessage(MOVECRAFT_COMMAND_PREFIX + I18nSupport.getInternationalisedString("Scuttle - Craft Already Sinking"));
+            return true;
+        }
+        craft.setCruising(false);
+        craft.sink();
+        CraftManager.getInstance().removePlayerFromCraft(craft);
+        commandSender.sendMessage(MOVECRAFT_COMMAND_PREFIX + I18nSupport.getInternationalisedString("Scuttle - Scuttle Activated"));
+        return true;
+
+    }
+}
+
