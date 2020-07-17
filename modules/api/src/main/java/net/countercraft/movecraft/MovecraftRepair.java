f79d6c8321e45b30dbac70862968ce2c3437b821
TylerS1066 <TylerS1066@users.noreply.github.com>
2020-07-17 14:29:24 -0500

    Removed final assault parts from core plugin

--- modules/api/src/main/java/net/countercraft/movecraft/MovecraftRepair.java
+++ modules/api/src/main/java/net/countercraft/movecraft/MovecraftRepair.java
@@ -19,12 +19,7 @@ import com.sk89q.worldedit.function.mask.BlockMask;
 import com.sk89q.worldedit.function.operation.ForwardExtentCopy;
 import com.sk89q.worldedit.function.operation.Operations;
 import com.sk89q.worldedit.regions.CuboidRegion;
-import com.sk89q.worldedit.regions.Polygonal2DRegion;
-import com.sk89q.worldedit.regions.Region;
 import com.sk89q.worldedit.world.registry.WorldData;
-import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
-import com.sk89q.worldguard.protection.regions.ProtectedPolygonalRegion;
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import net.countercraft.movecraft.config.Settings;
 import net.countercraft.movecraft.craft.Craft;
 import net.countercraft.movecraft.utils.*;
@@ -103,60 +98,6 @@ public class MovecraftRepair {
         }
 
 
-    }
-
-    public boolean saveRegionRepairState(World world, ProtectedRegion region) {
-
-        File saveDirectory = new File(plugin.getDataFolder(), "AssaultSnapshots");
-        com.sk89q.worldedit.world.World weWorld = new BukkitWorld(world);
-        WorldData worldData = weWorld.getWorldData();
-        Vector weMinPos = region.getMinimumPoint();
-        Vector weMaxPos = region.getMaximumPoint();
-        if (!saveDirectory.exists()) {
-            saveDirectory.mkdirs();
-        }
-        Set<BaseBlock> baseBlockSet = new HashSet<>();
-        Region weRegion = null;
-        if (region instanceof ProtectedCuboidRegion) {
-            weRegion = new CuboidRegion(weMinPos, weMaxPos);
-        } else if (region instanceof ProtectedPolygonalRegion) {
-            ProtectedPolygonalRegion polyReg = (ProtectedPolygonalRegion) region;
-            weRegion = new Polygonal2DRegion(weWorld, polyReg.getPoints(), polyReg.getMinimumPoint().getBlockY(), polyReg.getMaximumPoint().getBlockY());
-        }
-
-
-        File repairStateFile = new File(saveDirectory, region.getId().replaceAll("´\\s+", "_") + ".schematic");
-        for (int x = weMinPos.getBlockX(); x <= weMaxPos.getBlockX(); x++) {
-            for (int y = weMinPos.getBlockY(); y <= weMaxPos.getBlockY(); y++) {
-                for (int z = weMinPos.getBlockZ(); z <= weMaxPos.getBlockZ(); z++) {
-                    Block block = world.getBlockAt(x, y, z);
-                    if (block.getType().equals(Material.AIR)) {
-                        continue;
-                    }
-                    if (Settings.AssaultDestroyableBlocks.contains(block.getTypeId())) {
-                        baseBlockSet.add(new BaseBlock(block.getTypeId(), block.getData()));
-                    }
-                }
-            }
-        }
-        try {
-
-            BlockArrayClipboard clipboard = new BlockArrayClipboard(weRegion);
-            Extent source = WorldEdit.getInstance().getEditSessionFactory().getEditSession(weWorld, -1);
-            Extent destination = clipboard;
-            ForwardExtentCopy copy = new ForwardExtentCopy(source, weRegion, clipboard.getOrigin(), destination, weMinPos);
-            BlockMask mask = new BlockMask(source, baseBlockSet);
-            copy.setSourceMask(mask);
-            Operations.completeLegacy(copy);
-            ClipboardWriter writer = ClipboardFormat.SCHEMATIC.getWriter(new FileOutputStream(repairStateFile, false));
-            writer.write(clipboard, worldData);
-            writer.close();
-            return true;
-
-        } catch (MaxChangedBlocksException | IOException e) {
-            e.printStackTrace();
-            return false;
-        }
     }
 
     public Clipboard loadCraftRepairStateClipboard(Craft craft, Sign sign) {
@@ -560,20 +501,6 @@ public class MovecraftRepair {
         return clipboard;
     }
 
-    public Clipboard loadRegionRepairStateClipboard(String s, World world) {
-        File dataDirectory = new File(plugin.getDataFolder(), "AssaultSnapshots");
-        File file = new File(dataDirectory, s + ".schematic"); // The schematic file
-        com.sk89q.worldedit.world.World weWorld = new BukkitWorld(world);
-        WorldData worldData = weWorld.getWorldData();
-        try {
-            return ClipboardFormat.SCHEMATIC.getReader(new FileInputStream(file)).read(worldData);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
     public HashMap<Pair<Material, Byte>, Double> getMissingBlocks(String repairName) {
         return missingBlocksMap.get(repairName);
     }
@@ -617,7 +544,6 @@ public class MovecraftRepair {
         return returnSet;
     }
 
-
     public static void initialize(Plugin plugin){
         instance = new MovecraftRepair(plugin);
     }
@@ -625,6 +551,4 @@ public class MovecraftRepair {
     public static MovecraftRepair getInstance() {
         return instance;
     }
-
-
 }
