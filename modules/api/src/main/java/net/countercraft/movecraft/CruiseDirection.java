c787da45090cde946363c56383a07e6751b0cdfb
HumorousFool <35515385+humorousfool@users.noreply.github.com>
2021-02-09 10:09:46 -0800

    Put byte values in CruiseDirection

--- modules/api/src/main/java/net/countercraft/movecraft/CruiseDirection.java
+++ modules/api/src/main/java/net/countercraft/movecraft/CruiseDirection.java
@@ -3,16 +3,41 @@ package net.countercraft.movecraft;
 import org.bukkit.block.BlockFace;
 
 public enum CruiseDirection {
-    NORTH, //0x3
-    SOUTH, //0x2
-    EAST, //0x4
-    WEST, //0x5
-    UP, //0x42
-    DOWN, //0x43
-    NONE;
+    NORTH((byte) 0x3), //0x3
+    SOUTH((byte) 0x2), //0x2
+    EAST((byte) 0x4), //0x4
+    WEST((byte) 0x5), //0x5
+    UP((byte) 0x42), //0x42
+    DOWN((byte) 0x43), //0x43
+    NONE((byte) 0x0);
 
-    public static CruiseDirection fromBlockFace(BlockFace direction)
-    {
+    private final byte raw;
+
+    CruiseDirection(byte rawDirection) {
+        raw = rawDirection;
+    }
+
+    public byte getRaw() {
+        return raw;
+    }
+    public static CruiseDirection fromRaw(byte rawDirection) {
+        if(rawDirection == (byte) 0x3)
+            return NORTH;
+        else if(rawDirection == (byte) 0x2)
+            return SOUTH;
+        else if(rawDirection == (byte) 0x4)
+            return EAST;
+        else if(rawDirection == (byte) 0x5)
+            return WEST;
+        else if(rawDirection == (byte) 0x42)
+            return UP;
+        else if(rawDirection == (byte) 0x43)
+            return DOWN;
+        else
+            return NONE;
+    }
+
+    public static CruiseDirection fromBlockFace(BlockFace direction) {
         if(direction.getOppositeFace() == BlockFace.NORTH)
             return NORTH;
         else if(direction.getOppositeFace() == BlockFace.SOUTH)
