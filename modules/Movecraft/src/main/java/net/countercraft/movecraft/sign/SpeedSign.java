5ba30604754b16610fdbe8f4283b17b259ae3434
Nicole <52045376+drfiveminusmint@users.noreply.github.com>
2021-03-06 14:07:47 -0500

    Gear changes
    Moves gear control to right-clicking the speed sign to improve ergonomics.

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/sign/SpeedSign.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/sign/SpeedSign.java
@@ -2,15 +2,20 @@ package net.countercraft.movecraft.sign;
 
 import net.countercraft.movecraft.MovecraftLocation;
 import net.countercraft.movecraft.craft.Craft;
+import net.countercraft.movecraft.craft.CraftManager;
 import net.countercraft.movecraft.events.CraftDetectEvent;
 import net.countercraft.movecraft.events.SignTranslateEvent;
+import net.countercraft.movecraft.localisation.I18nSupport;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public final class SpeedSign implements Listener{
     @EventHandler
@@ -39,5 +44,37 @@ public final class SpeedSign implements Listener{
         event.setLine(1,String.format("%.2f",craft.getSpeed()) + "m/s");
         event.setLine(2,String.format("%.2f",craft.getMeanCruiseTime() * 1000) + "ms");
         event.setLine(3,craft.getTickCooldown() + "T");
+    }
+
+    @EventHandler
+    public void onSideClick(PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
+        Block block = event.getClickedBlock();
+        if (block.getType() != Material.SIGN_POST && block.getType() != Material.WALL_SIGN) {
+            return;
+        }
+        Sign sign = (Sign) event.getClickedBlock().getState();
+        if (!ChatColor.stripColor(sign.getLine(0)).equalsIgnoreCase("Speed:")) {
+            return;
+        }
+        Player player = event.getPlayer();
+        Craft craft = CraftManager.getInstance().getCraftByPlayer(player);
+        if (craft == null) {
+            return;
+        }
+        final int gearShifts = craft.getType().getGearShifts();
+        int currentGear = craft.getCurrentGear();
+        if (gearShifts == 1) {
+            player.sendMessage(I18nSupport.getInternationalisedString("Gearshift - Disabled for craft type"));
+            return;
+        }
+        currentGear++;
+        if (currentGear > gearShifts)
+            currentGear = 1;
+        player.sendMessage(I18nSupport.getInternationalisedString("Gearshift - Gear changed") + " " + currentGear + " / " + gearShifts);
+        craft.setCurrentGear(currentGear);
+        return;
     }
 }
