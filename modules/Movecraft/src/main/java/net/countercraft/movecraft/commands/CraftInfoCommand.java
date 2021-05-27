c017d9f6be4923f7ca834f13e966bef5dfefba07
cccm5 <cccm5dev@gmail.com>
2021-05-27 14:27:34 -0400

    craftinfo command nullability annotations

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/commands/CraftInfoCommand.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/commands/CraftInfoCommand.java
@@ -2,7 +2,6 @@ package net.countercraft.movecraft.commands;
 
 import net.countercraft.movecraft.craft.Craft;
 import net.countercraft.movecraft.craft.CraftManager;
-import net.countercraft.movecraft.craft.CraftType;
 import net.countercraft.movecraft.util.MathUtils;
 import net.countercraft.movecraft.util.TopicPaginator;
 import org.bukkit.command.Command;
@@ -12,14 +11,12 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.OptionalInt;
-import java.util.Set;
 import java.util.function.Function;
 
 public class CraftInfoCommand implements TabExecutor {
-    private static List<Function<Craft, ? extends Iterable<String>>> providers = new ArrayList<>();
+    private static final List<Function<@NotNull Craft, @NotNull ? extends Iterable<String>>> providers = new ArrayList<>();
     static {
         registerMultiProvider(CraftInfoCommand::allowedBlockProvider);
         registerProvider((craft -> "Craft size: " + craft.getHitBox().size()));
@@ -41,15 +38,15 @@ public class CraftInfoCommand implements TabExecutor {
         registerProvider((craft -> "Current gear: " + craft.getCurrentGear()));
     }
 
-    private static List<String> allowedBlockProvider(Craft craft){
+    private static @NotNull List<String> allowedBlockProvider(@NotNull Craft craft){
         return List.of();
     }
 
-    public static void registerMultiProvider(Function<Craft, ? extends Iterable<String>> provider){
+    public static void registerMultiProvider(@NotNull Function<@NotNull Craft, @NotNull ? extends Iterable<String>> provider){
         providers.add(provider);
     }
 
-    public static void registerProvider(Function<Craft, String> provider){
+    public static void registerProvider(@NotNull Function<@NotNull Craft, @NotNull String> provider){
         providers.add(provider.andThen(List::of));
     }
 
@@ -57,16 +54,26 @@ public class CraftInfoCommand implements TabExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if(args.length == 0){
-            if (sender instanceof  Player){
-                craftInfo(sender, CraftManager.getInstance().getCraftByPlayer(((Player) sender)), 1);
+            if (!(sender instanceof Player)){
+                sender.sendMessage("Supply a parameter.");
                 return true;
             }
-            sender.sendMessage("Supply a parameter");
+            Craft craft = CraftManager.getInstance().getCraftByPlayer(((Player) sender));
+            if(craft == null){
+                sender.sendMessage("You must be piloting a craft.");
+                return true;
+            }
+            craftInfo(sender, craft, 1);
             return true;
         }
         OptionalInt pageQuery;
         if (sender instanceof  Player && (pageQuery = MathUtils.parseInt(args[0])).isPresent()){
-            craftInfo(sender, CraftManager.getInstance().getCraftByPlayer(((Player) sender)), pageQuery.getAsInt());
+            Craft craft = CraftManager.getInstance().getCraftByPlayer(((Player) sender));
+            if(craft == null){
+                sender.sendMessage("You must be piloting a craft.");
+                return true;
+            }
+            craftInfo(sender, craft, pageQuery.getAsInt());
             return true;
         }
         var craft = CraftManager.getInstance().getCraftByPlayerName(args[0]);
@@ -87,7 +94,7 @@ public class CraftInfoCommand implements TabExecutor {
         return true;
     }
 
-    public void craftInfo(CommandSender commandSender, Craft craft, int page){
+    public void craftInfo(@NotNull CommandSender commandSender, @NotNull Craft craft, int page){
         TopicPaginator paginator = new TopicPaginator("Craft Info");
         for(var provider : providers){
             for(var line : provider.apply(craft)){
@@ -103,7 +110,7 @@ public class CraftInfoCommand implements TabExecutor {
     }
 
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
         return null;
     }
 }
