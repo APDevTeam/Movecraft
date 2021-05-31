257b4656f4a27bc2576372f160b4187a905ee3d3
cccm5 <cccm5dev@gmail.com>
2021-05-31 17:20:21 -0400

    1.14.4 suppourt

---
+++ modules/v1_14_R1/src/main/java/net/countercraft/movecraft/compat/v1_14_R1/NextTickProvider.java
@@ -0,0 +1,31 @@
+package net.countercraft.movecraft.compat.v1_14_R1;
+
+import net.minecraft.server.v1_14_R1.BlockPosition;
+import net.minecraft.server.v1_14_R1.NextTickListEntry;
+import net.minecraft.server.v1_14_R1.WorldServer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class NextTickProvider {
+
+    @Nullable
+    public NextTickListEntry<?> getNextTick(@NotNull WorldServer world,@NotNull BlockPosition position){
+        return null;
+    }
+    @NotNull
+    public Object fakeEntry(@NotNull BlockPosition position){
+        return new Object(){
+            @Override
+            public int hashCode() {
+                return position.hashCode();
+            }
+            @Override
+            public boolean equals(Object other){
+                if (!(other instanceof NextTickListEntry)) {
+                    return false;
+                }
+                return position.equals(((NextTickListEntry<?>)other).a);
+            }
+        };
+    }
+}
