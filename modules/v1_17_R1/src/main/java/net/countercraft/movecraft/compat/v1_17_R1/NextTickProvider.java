eb57cfcc5d314db1460f1dfcad2141b2621d52a8
cccm5 <cccm5dev@gmail.com>
2021-06-21 17:08:47 -0400

    Update NextTickProvider

--- modules/v1_17_R1/src/main/java/net/countercraft/movecraft/compat/v1_17_R1/NextTickProvider.java
+++ modules/v1_17_R1/src/main/java/net/countercraft/movecraft/compat/v1_17_R1/NextTickProvider.java
@@ -2,30 +2,29 @@ package net.countercraft.movecraft.compat.v1_17_R1;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.ServerTickList;
 import net.minecraft.world.level.TickNextTickData;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.logging.Logger;
 
 public class NextTickProvider {
 
     @Nullable
     public TickNextTickData<?> getNextTick(@NotNull ServerLevel world, @NotNull BlockPos position){
+        ServerTickList<Block> tickList = world.getBlockTickList();
+        var box = BoundingBox.encapsulatingPositions(List.of(position));
+        if(box.isEmpty()){
+            return null;
+        }
+        for(var tick : tickList.fetchTicksInArea(box.get(), false, false)){
+            Logger.getAnonymousLogger().info("" + tick);
+            return tick;
+        }
         return null;
-    }
-    @NotNull
-    public Object fakeEntry(@NotNull BlockPos position){
-        return new Object(){
-            @Override
-            public int hashCode() {
-                return position.hashCode();
-            }
-            @Override
-            public boolean equals(Object other){
-                if (!(other instanceof TickNextTickData)) {
-                    return false;
-                }
-                return position.equals(((TickNextTickData<?>)other).pos);
-            }
-        };
     }
 }
