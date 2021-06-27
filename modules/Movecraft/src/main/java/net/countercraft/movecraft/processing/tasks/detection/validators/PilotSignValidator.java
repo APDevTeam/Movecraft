0a42eb519aada1d91f73a3c690354c586bd9cec3
cccm5 <cccm5dev@gmail.com>
2021-06-27 09:29:25 -0400

    Prevent unnecessary synchronization for sign lookups with mismatched types

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/processing/tasks/detection/validators/PilotSignValidator.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/processing/tasks/detection/validators/PilotSignValidator.java
@@ -6,6 +6,7 @@ import net.countercraft.movecraft.localisation.I18nSupport;
 import net.countercraft.movecraft.processing.MovecraftWorld;
 import net.countercraft.movecraft.processing.functions.Result;
 import net.countercraft.movecraft.processing.functions.DetectionPredicate;
+import org.bukkit.Tag;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
 import org.bukkit.command.CommandSender;
@@ -17,6 +18,9 @@ public class PilotSignValidator implements DetectionPredicate<MovecraftLocation>
     @Override
     @Contract(pure = true)
     public @NotNull Result validate(@NotNull MovecraftLocation location, @NotNull CraftType type, @NotNull MovecraftWorld world, @Nullable CommandSender player) {
+        if(!Tag.SIGNS.isTagged(world.getMaterial(location))){
+            return Result.succeed();
+        }
         BlockState state = world.getState(location);
         if (!(state instanceof Sign)) {
             return Result.succeed();
