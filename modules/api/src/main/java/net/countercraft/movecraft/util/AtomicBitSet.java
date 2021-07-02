67a2a0ed2573eb6bb2de1a51f66c39dac98b6b56
cccm5 <cccm5dev@gmail.com>
2021-07-02 10:09:30 -0400

    Enable padding when permitted via jvm flags

--- modules/api/src/main/java/net/countercraft/movecraft/util/AtomicBitSet.java
+++ modules/api/src/main/java/net/countercraft/movecraft/util/AtomicBitSet.java
@@ -7,12 +7,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AtomicBitSet {
-    private static final VarHandle BACKING = MethodHandles.arrayElementVarHandle(long[].class);
+    private static final VarHandle BACKING = MethodHandles.arrayElementVarHandle(Cell[].class);
+    private static final VarHandle VALUE;
+
+    static {
+        try {
+            VALUE = MethodHandles.lookup().findVarHandle(Cell.class, "value", long.class);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private static final int FIELD_LENGTH = 64;
-    private final long[] backing;
+    private final Cell[] backing;
 
     public AtomicBitSet(int maxSize){
-        this.backing = new long[(int) Math.ceil(maxSize/(double)FIELD_LENGTH)];
+        this.backing = new Cell[(int) Math.ceil(maxSize/(double)FIELD_LENGTH)];
     }
 
     /**
@@ -22,9 +32,10 @@ public class AtomicBitSet {
      */
     public boolean add(int index){
         while(true){
-            long bitField = (long) BACKING.getAcquire(backing, index/FIELD_LENGTH);
-            if(BACKING.weakCompareAndSetRelease(backing, index/FIELD_LENGTH, bitField,
-                    bitField | (1L << (index & (FIELD_LENGTH - 1))))){
+            var cell = getCell(index);
+            long bitField = (long) VALUE.getAcquire(cell);
+            if(VALUE.weakCompareAndSetRelease(cell, bitField,
+                    bitField | (1L << (index & (FIELD_LENGTH - 1))))) {
                 return ((bitField >>> (index & (FIELD_LENGTH - 1))) & 1L) == 1;
             }
         }
@@ -37,7 +48,20 @@ public class AtomicBitSet {
      */
     public boolean get(int index){
         // Use acquire to ensure a happens before relation in external contexts
-        return (((long) BACKING.getAcquire(backing, index/FIELD_LENGTH) >>> (index & (FIELD_LENGTH - 1))) & 1L) == 1;
+        return ((((long) VALUE.getAcquire(getCell(index))) >>> (index & (FIELD_LENGTH - 1))) & 1L) == 1;
+    }
+
+    private Cell getCell(int index){
+        Cell cellField = (Cell) BACKING.getAcquire(backing, index/FIELD_LENGTH);
+        if (cellField != null) {
+            return cellField;
+        }
+        cellField = new Cell();
+
+        if(BACKING.compareAndExchangeRelease(backing, index/FIELD_LENGTH, null, cellField) == null){
+            return cellField;
+        }
+        return (Cell) BACKING.getAcquire(backing, index/FIELD_LENGTH);
     }
 
     @Override
@@ -51,5 +75,9 @@ public class AtomicBitSet {
             }
         }
         return "AtomicBitSet{" + String.join(",", values) + "}";
+    }
+
+    private static class Cell{
+        @jdk.internal.vm.annotation.Contended long value;
     }
 }
