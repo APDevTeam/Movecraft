e01a1ebac98fe1c7c2af7ebd8ea3bb74cfb3d76f
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-11-05 15:42:07 -0500

    Add to functional API

---
+++ modules/api/src/main/java/net/countercraft/movecraft/util/functions/QuadFunction.java
@@ -0,0 +1,53 @@
+package net.countercraft.movecraft.util.functions;
+
+import java.util.Objects;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/**
+ * Represents a function that accepts four arguments and produces a result.
+ * This is the four-arity specialization of {@link BiFunction}.
+ *
+ * <p>This is a <a href="package-summary.html">functional interface</a>
+ * whose functional method is {@link #apply(Object, Object, Object, Object)}.
+ *
+ * @param <T> the type of the first argument to the function
+ * @param <U> the type of the second argument to the function
+ * @param <V> The type of the third argument to the function
+ * @param <W> the type of the fourth argument to the function
+ * @param <R> the type of the result of the function
+ *
+ * @see BiFunction
+ */
+@FunctionalInterface
+public interface QuadFunction<T, U, V, W, R> {
+
+    /**
+     * Applies this function to the given arguments.
+     *
+     * @param t the first function argument
+     * @param u the second function argument
+     * @param v the third function argument
+     * @param w the fourth function argument
+     * @return the function result
+     */
+    R apply(T t, U u, V v, W w);
+
+    /**
+     * Returns a composed function that first applies this function to
+     * its input, and then applies the {@code after} function to the result.
+     * If evaluation of either function throws an exception, it is relayed to
+     * the caller of the composed function.
+     *
+     * @param <Z> the type of output of the {@code after} function, and of the
+     *           composed function
+     * @param after the function to apply after this function is applied
+     * @return a composed function that first applies this function and then
+     * applies the {@code after} function
+     * @throws NullPointerException if after is null
+     */
+    default <Z> QuadFunction<T, U, V, W, Z> andThen(Function<? super R, ? extends Z> after) {
+        Objects.requireNonNull(after);
+        return (T t, U u, V v, W w) -> after.apply(apply(t, u, v, w));
+    }
+}
