a88ef1bfe08786e3c552cceb6e32d72a36118af4
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-11-13 06:01:19 -0600

    add debug messages

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/processing/tasks/detection/validators/SubcraftValidator.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/processing/tasks/detection/validators/SubcraftValidator.java
@@ -1,8 +1,10 @@
 package net.countercraft.movecraft.processing.tasks.detection.validators;
 
+import net.countercraft.movecraft.Movecraft;
 import net.countercraft.movecraft.MovecraftLocation;
 import net.countercraft.movecraft.craft.Craft;
 import net.countercraft.movecraft.craft.CraftManager;
+import net.countercraft.movecraft.craft.PlayerCraft;
 import net.countercraft.movecraft.craft.type.CraftType;
 import net.countercraft.movecraft.localisation.I18nSupport;
 import net.countercraft.movecraft.processing.MovecraftWorld;
@@ -22,13 +24,25 @@ public class SubcraftValidator implements DetectionPredicate<MovecraftLocation> 
             return Result.succeed();
         }
 
-        for (Craft otherCraft : CraftManager.getInstance().getCraftList()) {
+        for (Craft otherCraft : CraftManager.getInstance()) {
+            Movecraft.getInstance().getLogger().info(
+                    "Checking against craft of type "
+                            + otherCraft.getType()
+                            + " piloted by "
+                            + ((otherCraft instanceof PlayerCraft) ? ((PlayerCraft) otherCraft).getPlayer().getDisplayName() : "null")
+                            + " at "
+                            + otherCraft.getHitBox().getMidPoint()
+            );
+
             if (!otherCraft.getMovecraftWorld().equals(world)) {
+                Movecraft.getInstance().getLogger().info("Wrong world!\t" + otherCraft.getMovecraftWorld().getName());
                 continue;
             }
             if (otherCraft.getHitBox().contains(movecraftLocation)) {
+                Movecraft.getInstance().getLogger().info("Hitbox is contained!");
                 return Result.succeed();
             }
+            Movecraft.getInstance().getLogger().info("Hitbox is not contained.");
         }
         return Result.failWithMessage(I18nSupport.getInternationalisedString("Detection - Must Be Subcraft"));
     }
