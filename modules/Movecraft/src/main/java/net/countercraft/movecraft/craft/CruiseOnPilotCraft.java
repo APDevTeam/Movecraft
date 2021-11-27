9d4557dedaaa893ad75a4cac89c5dc4b8726c275
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-11-27 10:23:22 -0600

    Update CruiseOnPilotCraft.java

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/craft/CruiseOnPilotCraft.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/craft/CruiseOnPilotCraft.java
@@ -2,11 +2,19 @@ package net.countercraft.movecraft.craft;
 
 import net.countercraft.movecraft.craft.type.CraftType;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public class CruiseOnPilotCraft extends BaseCraft {
+public class CruiseOnPilotCraft extends ICraft implements PilotedCraft {
+    private final Player pilot;
 
-    public CruiseOnPilotCraft(@NotNull CraftType type, @NotNull World world) {
+    public CruiseOnPilotCraft(@NotNull CraftType type, @NotNull World world, @NotNull Player pilot) {
         super(type, world);
+        this.pilot = pilot;
+    }
+
+    @Override
+    public @NotNull Player getPilot() {
+        return pilot;
     }
 }
