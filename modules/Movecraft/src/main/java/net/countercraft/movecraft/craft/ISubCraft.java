eeee8f0e9c58b8bf411a21759db0fdc639077c06
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-11-29 19:17:33 -0600

    Add parent to ISubCraft

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/craft/ISubCraft.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/craft/ISubCraft.java
@@ -5,13 +5,15 @@ import org.bukkit.World;
 import org.jetbrains.annotations.NotNull;
 
 public class ISubCraft extends ICraft implements SubCraft {
+    private final Craft parent;
 
-    public ISubCraft(@NotNull CraftType type, @NotNull World world) {
+    public ISubCraft(@NotNull CraftType type, @NotNull World world, @NotNull Craft parent) {
         super(type, world);
+        this.parent = parent;
     }
 
     @Override
     public @NotNull Craft getParent() {
-        return null;
+        return parent;
     }
 }
