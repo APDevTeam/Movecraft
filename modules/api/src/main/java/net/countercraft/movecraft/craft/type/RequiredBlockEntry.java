fc0dfa81d842f4f240d60b6983cf3542376714fd
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-12-16 17:28:45 -0600

    Document RequiredBlockEntry

--- modules/api/src/main/java/net/countercraft/movecraft/craft/type/RequiredBlockEntry.java
+++ modules/api/src/main/java/net/countercraft/movecraft/craft/type/RequiredBlockEntry.java
@@ -11,12 +11,23 @@ import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * This class represents a single flyblock or moveblock entry.
+ * <p>
+ * It contains a set of materials which it applies to.
+ * <p>
+ * It also contains variables to determine the min and max.
+ * <p>
+ * A numeric limit represents a configured static limit (regardless of size), ex: "N10" for 10 blocks.<br>
+ * A percentage limit represents a limit which scales with craft size, ex: 10 for 10%.
+ */
 public class RequiredBlockEntry {
     private final EnumSet<Material> materials;
     private final double max;
     private final boolean numericMax;
     private final double min;
     private final boolean numericMin;
+
 
     public RequiredBlockEntry(EnumSet<Material> materials, @NotNull Pair<Boolean, ? extends Number> min, @NotNull Pair<Boolean, ? extends Number> max) {
         this.materials = materials;
@@ -26,14 +37,30 @@ public class RequiredBlockEntry {
         this.numericMax = max.getLeft();
     }
 
+    /**
+     * Check if this <code>RequiredBlockEntry</code> contains a material.
+     *
+     * @param m Material to check
+     * @return <code>true</code> if this contains the material, <code>false</code> if it does not
+     */
     public boolean contains(Material m) {
         return materials.contains(m);
     }
 
+    /**
+     * Get a set of the materials this <code>RequiredBlockEntry</code> contains.
+     *
+     * @return A copy of the materials this contains
+     */
     public Set<Material> getMaterials() {
         return Collections.unmodifiableSet(materials);
     }
 
+    /**
+     * Convert the materials contained in this to a string.
+     *
+     * @return A string representation of the materials contained in this
+     */
     public String materialsToString() {
         Set<String> names = new HashSet<>();
         for(Material m : materials) {
@@ -42,10 +69,25 @@ public class RequiredBlockEntry {
         return String.join(", ", names);
     }
 
+    /**
+     * Check a block count and size against this
+     *
+     * @param count Count of this entry on the craft
+     * @param size Size of the craft
+     * @return <code>true</code> if the count and size pass the min and max bounds, <code>false</code> if it does not
+     */
     public boolean check(int count, int size) {
         return check(count, size, 1.0D);
     }
 
+    /**
+     * Check a block count and size against this with the specified sinkPercent
+     *
+     * @param count Count of this entry on the craft
+     * @param size Size of the craft
+     * @param sinkPercent The allowed percentage below the max
+     * @return <code>true</code> if the count and size pass the min and max bounds, <code>false</code> if it does not
+     */
     public boolean check(int count, int size, double sinkPercent) {
         double blockPercent = 100D * (double) count / size;
         if(numericMin) {
@@ -70,6 +112,13 @@ public class RequiredBlockEntry {
         SUCCESS
     }
 
+    /**
+     * Check a block count and size against this and return the result with a string
+     *
+     * @param count Count of this entry on the craft
+     * @param size Size of the craft
+     * @return A pair with the result (not enough, too much, or success) and the reason for failure (or blank for success)
+     */
     public Pair<DetectionResult, String> detect(int count, int size) {
         double blockPercent = 100D * (double) count / size;
 
@@ -86,18 +135,42 @@ public class RequiredBlockEntry {
         return new Pair<>(DetectionResult.SUCCESS, "");
     }
 
+    /**
+     * Get the max for this <code>RequiredBlockEntry</code>
+     * @return
+     * If this is a numeric max, this returns the integer value (N1 = 1.0D).
+     * If this is not a numeric max, this returns the percentage (1.0D = 100%)
+     */
     public double getMax() {
         return max;
     }
 
+    /**
+     * Check if this has a numeric or percentage max
+     * @return
+     * If this is a numeric max, this returns <code>true</code>
+     * If this is not a numeric max, this returns <code>false</code>
+     */
     public boolean isNumericMax() {
         return numericMax;
     }
 
+    /**
+     * Get the min for this <code>RequiredBlockEntry</code>
+     * @return
+     * If this is a numeric min, this returns the integer value (N1 = 1.0D).
+     * If this is not a numeric min, this returns the percentage (0.5D = 50%)
+     */
     public double getMin() {
         return min;
     }
 
+    /**
+     * Check if this has a numeric or percentage min
+     * @return
+     * If this is a numeric min, this returns <code>true</code>
+     * If this is not a numeric min, this returns <code>false</code>
+     */
     public boolean isNumericMin() {
         return numericMin;
     }
