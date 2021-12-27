611b8b48614ad34070e442694c602c8dd6e0cea5
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-12-27 11:26:11 -0600

    Convert CraftSupplier from a Collection to a Set

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/processing/functions/CraftSupplier.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/processing/functions/CraftSupplier.java
@@ -9,14 +9,14 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
+import java.util.Set;
 
 /**
  * Represents a function that accepts the outputs of detection and produces a Craft.
  * @see QuadFunction
  */
 @FunctionalInterface
-public interface CraftSupplier extends QuadFunction<CraftType, World, Player, Collection<Craft>, Pair<Result, Craft>> {
+public interface CraftSupplier extends QuadFunction<CraftType, World, Player, Set<Craft>, Pair<Result, Craft>> {
     /**
      * Applies this function to the given arguments.
      *
@@ -27,5 +27,5 @@ public interface CraftSupplier extends QuadFunction<CraftType, World, Player, Co
      * @return the result of construction and a possibly craft
      */
     @NotNull
-    Pair<@NotNull Result, @Nullable Craft> apply(@NotNull CraftType type, @NotNull World w, @NotNull Player player, @NotNull Collection<Craft> parents);
+    Pair<@NotNull Result, @Nullable Craft> apply(@NotNull CraftType type, @NotNull World w, @NotNull Player player, @NotNull Set<Craft> parents);
 }
