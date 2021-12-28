4d2df7854ec32206d53ffe39ad7e6494e5c85eed
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-12-28 12:25:19 -0600

    Cleanup SubcraftRotateSign

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/sign/SubcraftRotateSign.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/sign/SubcraftRotateSign.java
@@ -1,59 +1,47 @@
 package net.countercraft.movecraft.sign;
 
-import net.countercraft.movecraft.CruiseDirection;
 import net.countercraft.movecraft.Movecraft;
 import net.countercraft.movecraft.MovecraftLocation;
 import net.countercraft.movecraft.MovecraftRotation;
 import net.countercraft.movecraft.craft.Craft;
 import net.countercraft.movecraft.craft.CraftManager;
-import net.countercraft.movecraft.craft.BaseCraft;
 import net.countercraft.movecraft.craft.SubCraft;
+import net.countercraft.movecraft.craft.SubCraftImpl;
 import net.countercraft.movecraft.craft.SubcraftRotateCraft;
 import net.countercraft.movecraft.craft.type.CraftType;
-import net.countercraft.movecraft.craft.SubCraftImpl;
 import net.countercraft.movecraft.events.CraftPilotEvent;
 import net.countercraft.movecraft.events.CraftReleaseEvent;
 import net.countercraft.movecraft.localisation.I18nSupport;
-import net.countercraft.movecraft.processing.MovecraftWorld;
-import net.countercraft.movecraft.processing.effects.Effect;
-import net.countercraft.movecraft.processing.functions.CraftSupplier;
 import net.countercraft.movecraft.processing.functions.Result;
 import net.countercraft.movecraft.util.Pair;
-import net.countercraft.movecraft.util.hitboxes.HitBox;
-import net.countercraft.movecraft.util.hitboxes.MutableHitBox;
-import net.kyori.adventure.audience.Audience;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
-import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
-import java.util.function.Function;
 
 public final class SubcraftRotateSign implements Listener {
     private static final String HEADER = "Subcraft Rotate";
-    private final Set<MovecraftLocation> rotatingCrafts = new HashSet<>();
+    private final Set<MovecraftLocation> rotating = new HashSet<>();
 
-    @EventHandler
-    public void onSignClick(PlayerInteractEvent event) {
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+    public void onSignClick(@NotNull PlayerInteractEvent event) {
         MovecraftRotation rotation;
-        if(event.getAction() == Action.RIGHT_CLICK_BLOCK)
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK)
             rotation = MovecraftRotation.CLOCKWISE;
-        else if(event.getAction() == Action.LEFT_CLICK_BLOCK)
+        else if (event.getAction() == Action.LEFT_CLICK_BLOCK)
             rotation = MovecraftRotation.ANTICLOCKWISE;
         else
             return;
@@ -65,9 +53,9 @@ public final class SubcraftRotateSign implements Listener {
         if (!ChatColor.stripColor(sign.getLine(0)).equalsIgnoreCase(HEADER))
             return;
 
-        final Location loc = event.getClickedBlock().getLocation();
-        final MovecraftLocation startPoint = new MovecraftLocation(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
-        if(rotatingCrafts.contains(startPoint)) {
+        Location loc = event.getClickedBlock().getLocation();
+        MovecraftLocation startPoint = new MovecraftLocation(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+        if (rotating.contains(startPoint)) {
             event.getPlayer().sendMessage(I18nSupport.getInternationalisedString("Rotation - Already Rotating"));
             event.setCancelled(true);
             return;
@@ -76,22 +64,22 @@ public final class SubcraftRotateSign implements Listener {
         // rotate subcraft
         String craftTypeStr = ChatColor.stripColor(sign.getLine(1));
         CraftType craftType = CraftManager.getInstance().getCraftTypeFromString(craftTypeStr);
-        if(craftType == null)
+        if (craftType == null)
             return;
-        if(ChatColor.stripColor(sign.getLine(2)).equals("")
+        if (ChatColor.stripColor(sign.getLine(2)).equals("")
                 && ChatColor.stripColor(sign.getLine(3)).equals("")) {
             sign.setLine(2, "_\\ /_");
             sign.setLine(3, "/ \\");
             sign.update(false, false);
         }
 
-        if(!event.getPlayer().hasPermission("movecraft." + craftTypeStr + ".pilot") || !event.getPlayer().hasPermission("movecraft." + craftTypeStr + ".rotate")) {
+        if (!event.getPlayer().hasPermission("movecraft." + craftTypeStr + ".pilot") || !event.getPlayer().hasPermission("movecraft." + craftTypeStr + ".rotate")) {
             event.getPlayer().sendMessage(I18nSupport.getInternationalisedString("Insufficient Permissions"));
             return;
         }
 
-        final Craft playerCraft = CraftManager.getInstance().getCraftByPlayer(event.getPlayer());
-        if(playerCraft != null) {
+        Craft playerCraft = CraftManager.getInstance().getCraftByPlayer(event.getPlayer());
+        if (playerCraft != null) {
             if (!playerCraft.isNotProcessing()) {
                 event.getPlayer().sendMessage(I18nSupport.getInternationalisedString("Detection - Parent Craft is busy"));
                 return;
@@ -105,17 +93,17 @@ public final class SubcraftRotateSign implements Listener {
             }.runTaskLater(Movecraft.getInstance(), (10));
         }
 
-        rotatingCrafts.add(startPoint);
+        rotating.add(startPoint);
 
-        final Player player = event.getPlayer();
-        final World world = event.getClickedBlock().getWorld();
+        Player player = event.getPlayer();
+        World world = event.getClickedBlock().getWorld();
         CraftManager.getInstance().detect(
                 startPoint,
                 craftType, (type, w, p, parents) -> {
-                    if(parents.size() > 1)
+                    if (parents.size() > 1)
                         return new Pair<>(Result.failWithMessage(I18nSupport.getInternationalisedString(
                                 "Detection - Failed - Already commanding a craft")), null);
-                    if(parents.size() < 1)
+                    if (parents.size() < 1)
                         return new Pair<>(Result.succeed(), new SubcraftRotateCraft(type, w, p));
 
                     Craft parent = parents.iterator().next();
@@ -130,7 +118,6 @@ public final class SubcraftRotateSign implements Listener {
                         @Override
                         public void run() {
                             craft.rotate(rotation, startPoint, true);
-                            rotatingCrafts.remove(startPoint);
                             if (craft instanceof SubCraft) {
                                 SubCraft subcraft = (SubCraft) craft;
                                 var newHitbox = subcraft.getParent().getHitBox().union(craft.getHitBox());
@@ -145,7 +132,7 @@ public final class SubcraftRotateSign implements Listener {
         new BukkitRunnable() {
             @Override
             public void run() {
-                rotatingCrafts.remove(startPoint);
+                rotating.remove(startPoint);
             }
         }.runTaskLater(Movecraft.getInstance(), 4);
     }
