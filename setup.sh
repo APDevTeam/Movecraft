811e2ac8bd3a2f0872f0ad612e856b3a2205196a
TylerS1066 <TylerS1066@users.noreply.github.com>
2022-06-18 22:38:53 +0000

    Fix permission problem installing JDK-11

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ if [ -f /usr/lib/jvm/temurin-11-jdk-amd64/bin/java ]; then
     echo "Java 11 already installed"
 else
     sudo apt install -y wget apt-transport-https
-    wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /usr/share/keyrings/adoptium.asc
-    echo "deb [signed-by=/usr/share/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+    wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /usr/share/keyrings/adoptium.asc
+    echo "deb [signed-by=/usr/share/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
     sudo apt update
     sudo apt install -y temurin-11-jdk
 fi
