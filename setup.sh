379925af5989158312eed83e38b1d801bf156c15
TylerS1066 <TylerS1066@users.noreply.github.com>
2022-07-30 14:15:46 -0500

    1.19.1 Support (#531)
    * Initial attempt
    
    * Convert default types to #wool_carpets
    
    * Create wool_carpets.json
    
    * Revert type changes
    
    * Define #carpets for 1.19+

--- setup.sh
+++ setup.sh
@@ -54,11 +54,11 @@ else
     java -jar BuildTools.jar --rev 1.18.2 --remapped
 fi
 
-# Build 1.19
-if [ -f ~/.m2/repository/org/spigotmc/spigot/1.19-R0.1-SNAPSHOT/spigot-1.19-R0.1-SNAPSHOT.jar ]; then
-    echo "1.19 already exists, skipping build"
+# Build 1.19.1
+if [ -f ~/.m2/repository/org/spigotmc/spigot/1.19-R0.1-SNAPSHOT/spigot-1.19.1-R0.1-SNAPSHOT.jar ]; then
+    echo "1.19.1 already exists, skipping build"
 else
-    java -jar BuildTools.jar --rev 1.19 --remapped
+    java -jar BuildTools.jar --rev 1.19.1 --remapped
 fi
 
 # Restore git information
