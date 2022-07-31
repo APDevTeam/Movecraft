6003352aa34772c60953b4d0754409e31b2028ce
TylerS1066 <TylerS1066@users.noreply.github.com>
2022-07-31 10:25:28 -0500

    Cleanup and reorganize

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/util/TeleportUtils.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/util/TeleportUtils.java
@@ -5,14 +5,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -26,7 +25,6 @@ public class TeleportUtils {
     private static Constructor vec3D;
 
     private static Method position;
-    private static Method closeInventory;
     private static Method sendMethod;
 
     private static Field connectionField;
@@ -40,8 +38,6 @@ public class TeleportUtils {
     private static Field eField;
     private static Field yaw;
     private static Field pitch;
-    private static Field activeContainer;
-    private static Field defaultContainer;
     private static final boolean intialized;
 
     static {
@@ -50,20 +46,16 @@ public class TeleportUtils {
             Class<?> packet = getNmsClass("Packet");
             Class<?> entity = getNmsClass("Entity");
             Class<?> entityPlayer = getNmsClass("EntityPlayer");
-            Class<?> entityHuman = getNmsClass("EntityHuman");
             Class<?> connectionClass = getNmsClass("PlayerConnection");
             Class<?> packetClass = getNmsClass("PacketPlayOutPosition");
             Class<?> vecClass = getNmsClass("Vec3D");
             sendMethod = connectionClass.getMethod("sendPacket", packet);
 
             position = entity.getDeclaredMethod("setLocation", Double.TYPE, Double.TYPE, Double.TYPE, Float.TYPE, Float.TYPE);
-            closeInventory = entityPlayer.getDeclaredMethod("closeInventory");
 
             yaw = getField(entity, "yaw");
             pitch = getField(entity, "pitch");
             connectionField = getField(entityPlayer, "playerConnection");
-            activeContainer = getField(entityHuman, "activeContainer");
-            defaultContainer = getField(entityHuman, "defaultContainer");
 
             packetConstructor = packetClass.getConstructor(Double.TYPE, Double.TYPE, Double.TYPE, Float.TYPE, Float.TYPE, Set.class, Integer.TYPE);
             vec3D = vecClass.getConstructor(Double.TYPE, Double.TYPE, Double.TYPE);
@@ -80,7 +72,8 @@ public class TeleportUtils {
             AField = getField(connectionClass, "A");
             eField = getField(connectionClass, "e");
             sucess = true;
-        } catch (ClassNotFoundException | NoSuchFieldException | NoSuchMethodException | SecurityException e) {
+        }
+        catch (ClassNotFoundException | NoSuchFieldException | NoSuchMethodException | SecurityException e) {
             Bukkit.getLogger().warning("Failed to access internal teleportation handle, switching to fallback");
             Bukkit.getLogger().info("This is expected for 1.17 and up."); // TODO: Figure this out
             //e.printStackTrace();
@@ -88,14 +81,35 @@ public class TeleportUtils {
         intialized = sucess;
     }
 
-    private static Field getField(Class<?> clazz, String name) throws NoSuchFieldException {
+
+    private static @NotNull Field getField(@NotNull Class<?> clazz, String name) throws NoSuchFieldException {
         Field field = clazz.getDeclaredField(name);
         field.setAccessible(true);
         return field;
     }
 
+    private static @NotNull Class<?> getNmsClass(String name) throws ClassNotFoundException {
+        return Class.forName("net.minecraft.server." + getVersion() + "." + name);
+    }
+
+    private static @NotNull String getVersion() {
+        return Bukkit.getServer().getClass().getPackage().getName().substring(23);
+    }
+
+    private static @Nullable Object getHandle(Entity entity) {
+        try {
+            Method entity_getHandle = entity.getClass().getMethod("getHandle");
+            return entity_getHandle.invoke(entity);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
     public static void teleport(Player player, Location location, float yawChange) {
-        if(!intialized){
+        if (!intialized) {
             Movecraft.getInstance().getWorldHandler().addPlayerLocation(player, location.getX() - player.getLocation().getX(), location.getY() - player.getLocation().getY(), location.getZ() - player.getLocation().getZ(), yawChange, 0);
             return;
         }
@@ -112,13 +126,15 @@ public class TeleportUtils {
             lastPosYField.set(connection, y);
             lastPosZField.set(connection, z);
             int teleportAwait = teleportAwaitField.getInt(connection) + 1;
-            if (teleportAwait == 2147483647) teleportAwait = 0;
+            if (teleportAwait == Integer.MAX_VALUE)
+                teleportAwait = 0;
             teleportAwaitField.set(connection, teleportAwait);
             AField.set(connection, eField.get(connection));
 
             Object packet = packetConstructor.newInstance(x, y, z, yawChange, 0, teleportFlags, teleportAwait);
             sendPacket(packet, player);
-        } catch (IllegalAccessException | IllegalArgumentException | InstantiationException | InvocationTargetException e) {
+        }
+        catch (IllegalAccessException | IllegalArgumentException | InstantiationException | InvocationTargetException e) {
             e.printStackTrace();
         }
     }
@@ -128,25 +144,9 @@ public class TeleportUtils {
             Object handle = getHandle(p);
             Object pConnection = connectionField.get(handle);
             sendMethod.invoke(pConnection, packet);
-        } catch (Exception var9) {
+        }
+        catch (Exception var9) {
             var9.printStackTrace();
         }
-    }
-
-    private static Object getHandle(Entity entity) {
-        try {
-            Method entity_getHandle = entity.getClass().getMethod("getHandle");
-            return entity_getHandle.invoke(entity);
-        } catch (Exception var2) {
-            var2.printStackTrace();
-            return null;
-        }
-    }
-    private static Class<?> getNmsClass(String name) throws ClassNotFoundException {
-        return Class.forName("net.minecraft.server." + getVersion() + "." + name);
-    }
-
-    private static String getVersion() {
-        return Bukkit.getServer().getClass().getPackage().getName().substring(23);
     }
 }
