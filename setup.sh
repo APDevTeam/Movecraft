6fb31857d9c5aaf646fd95e8162443ba3caa4246
TylerS1066 <TylerS1066@users.noreply.github.com>
2022-09-09 23:29:56 +0000

    Update devcontainer to 1.19.2

--- setup.sh
+++ setup.sh
@@ -55,10 +55,10 @@ else
 fi
 
 # Build 1.19.1
-if [ -f ~/.m2/repository/org/spigotmc/spigot/1.19-R0.1-SNAPSHOT/spigot-1.19.1-R0.1-SNAPSHOT.jar ]; then
-    echo "1.19.1 already exists, skipping build"
+if [ -f ~/.m2/repository/org/spigotmc/spigot/1.19-R0.1-SNAPSHOT/spigot-1.19.2-R0.1-SNAPSHOT.jar ]; then
+    echo "1.19.2 already exists, skipping build"
 else
-    java -jar BuildTools.jar --rev 1.19.1 --remapped
+    java -jar BuildTools.jar --rev 1.19.2 --remapped
 fi
 
 # Restore git information
