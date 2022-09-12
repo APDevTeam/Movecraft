ca79bd633f1c06c9d5fcb06a5861d1f5fba86d0e
drfiveminusmint <unconfigured@null.spigotmc.org>
2022-09-12 12:34:27 -0400

    Moving Redstone 1.16.5

--- modules/v1_16_R3/src/main/java/net/countercraft/movecraft/compat/v1_16_R3/NextTickProvider.java
+++ modules/v1_16_R3/src/main/java/net/countercraft/movecraft/compat/v1_16_R3/NextTickProvider.java
@@ -2,30 +2,31 @@ package net.countercraft.movecraft.compat.v1_16_R3;
 
 import net.minecraft.server.v1_16_R3.BlockPosition;
 import net.minecraft.server.v1_16_R3.NextTickListEntry;
+import net.minecraft.server.v1_16_R3.StructureBoundingBox;
 import net.minecraft.server.v1_16_R3.WorldServer;
+import org.bukkit.craftbukkit.libs.org.apache.commons.lang3.tuple.ImmutablePair;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+
 public class NextTickProvider {
 
+    private Map<WorldServer,ImmutablePair<WeakReference<TreeSet<NextTickListEntry>>,WeakReference<List<NextTickListEntry>>>> tickMap = new HashMap<>();
+
     @Nullable
-    public NextTickListEntry<?> getNextTick(@NotNull WorldServer world,@NotNull BlockPosition position){
+    @SuppressWarnings("ConstantConditions")
+    public NextTickListEntry getNextTick(@NotNull WorldServer world,@NotNull BlockPosition position) {
+        BlockPosition position1 = new BlockPosition(position.getX() + 1, position.getY() + 1, position.getZ() + 1);
+        for (NextTickListEntry entry : world.getBlockTickList().a(new StructureBoundingBox(position, position1), false, true)) {
+            if (position.equals(entry.a)) {
+                return entry;
+            }
+        }
         return null;
-    }
-    @NotNull
-    public Object fakeEntry(@NotNull BlockPosition position){
-        return new Object(){
-            @Override
-            public int hashCode() {
-                return position.hashCode();
-            }
-            @Override
-            public boolean equals(Object other){
-                if (!(other instanceof NextTickListEntry)) {
-                    return false;
-                }
-                return position.equals(((NextTickListEntry<?>)other).a);
-            }
-        };
     }
 }
