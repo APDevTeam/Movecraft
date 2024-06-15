c7df8eed2a7313b07fce30585951096d9fe4e552
TylerS1066 <TylerS1066@users.noreply.github.com>
2024-06-15 18:02:21 -0500

    Add datapack build

---
+++ modules/datapack/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    base
+}
+
+val zipTask = tasks.register<Zip>("zipFolder") {
+    archiveFileName.set("movecraft-data.zip")
+    destinationDirectory.set(file("$buildDir/zip"))
+
+    from("src/main/resources/movecraft-data") {
+        include("**/*")  // Include all files and folders within the specified folder
+    }
+}
+
+tasks.build {
+    dependsOn(zipTask)
+}
+
+description = "Movecraft-datapack"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,8 @@ rootProject.name = "movecraft-parent"
 include(":movecraft-v1_18")
 include(":movecraft-v1_20")
 include(":movecraft-api")
+include(":datapack")
 project(":movecraft-v1_18").projectDir = file("modules/v1_18")
 project(":movecraft-v1_20").projectDir = file("modules/v1_20")
 project(":movecraft-api").projectDir = file("modules/api")
+project(":datapack").projectDir = file("modules/datapack")
