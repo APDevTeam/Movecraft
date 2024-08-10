17f3d1f25a4648aeff6fba3c1618474c4558f583
ohnoey <cccm5dev@gmail.com>
2024-08-09 21:57:13 -0700

    Composition over inheritance, concurrency

--- api/src/main/java/net/countercraft/movecraft/craft/datatag/CraftDataTagContainer.java
+++ api/src/main/java/net/countercraft/movecraft/craft/datatag/CraftDataTagContainer.java
@@ -3,21 +3,29 @@ package net.countercraft.movecraft.craft.datatag;
 import net.countercraft.movecraft.craft.Craft;
 import org.bukkit.NamespacedKey;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-public class CraftDataTagContainer extends HashMap<CraftDataTagKey<?>, Object> {
+public class CraftDataTagContainer {
+    private final @NotNull ConcurrentMap<@NotNull CraftDataTagKey<?>, @Nullable Object> _backing;
 
-    public static final Map<NamespacedKey, CraftDataTagKey<?>> REGISTERED_TAGS = new HashMap<>();
+    public CraftDataTagContainer(){
+        _backing = new ConcurrentHashMap<>();
+    }
+
+    public static final Map<@NotNull NamespacedKey, @NotNull CraftDataTagKey<?>> REGISTERED_TAGS = new HashMap<>();
 
     public static <T> @NotNull CraftDataTagKey<T> tryRegisterTagKey(final @NotNull NamespacedKey key, final @NotNull Function<Craft, T> supplier) throws IllegalArgumentException {
         if (REGISTERED_TAGS.containsKey(key)) {
             throw new IllegalArgumentException("Duplicate keys are not allowed!");
         } else {
-            CraftDataTagKey<T> result = new CraftDataTagKey<T>(key, supplier);
+            CraftDataTagKey<T> result = new CraftDataTagKey<>(key, supplier);
             REGISTERED_TAGS.put(key, result);
             return result;
         }
@@ -28,26 +36,26 @@ public class CraftDataTagContainer extends HashMap<CraftDataTagKey<?>, Object> {
             // TODO: Log error
             return null;
         }
-        T result = null;
-        if (!this.containsKey(tagKey)) {
+        T result;
+        if (!_backing.containsKey(tagKey)) {
             result = tagKey.createNew(craft);
-            this.put(tagKey, result);
+            _backing.put(tagKey, result);
         } else {
-            Object stored = this.getOrDefault(tagKey, tagKey.createNew(craft));
+            Object stored = _backing.getOrDefault(tagKey, tagKey.createNew(craft));
             try {
                 T temp = (T) stored;
                 result = temp;
             } catch (ClassCastException cce) {
                 // TODO: Log error
                 result = tagKey.createNew(craft);
-                this.put(tagKey, result);
+                _backing.put(tagKey, result);
             }
         }
         return result;
     }
 
     public <T> void set(CraftDataTagKey<T> tagKey, @NotNull T value) {
-        this.put(tagKey, value);
+        _backing.put(tagKey, value);
     }
 
 }
