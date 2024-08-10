e14178ad76c6cde470d11ad66d7b8f59d58c7d66
ohnoey <cccm5dev@gmail.com>
2024-08-10 11:52:08 -0700

    Fold repeated AndThen calls

--- api/src/main/java/net/countercraft/movecraft/processing/effects/Effect.java
+++ api/src/main/java/net/countercraft/movecraft/processing/effects/Effect.java
@@ -1,7 +1,11 @@
 package net.countercraft.movecraft.processing.effects;
 
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @FunctionalInterface
 public interface Effect {
@@ -32,12 +36,44 @@ public interface Effect {
     }
 
     default @NotNull Effect andThen(@Nullable Effect chain){
-        if(chain == null){
+        return new AndEffect(this, chain);
+    }
+
+    class AndEffect implements Effect {
+        private final List<Effect> effects = new ArrayList<>();
+
+        public AndEffect(Effect... effects){
+            for (Effect effect : effects) {
+                andThen(effect);
+            }
+        }
+
+        @Override
+        public void run() {
+            effects.forEach(Effect::run);
+        }
+
+        @Override
+        public @NotNull Effect andThen(@Nullable Effect chain) {
+            if(this == chain){
+                // copy if chaining to self to prevent concurrent modification
+                effects.addAll(effects.stream().toList());
+
+                return this;
+            } else if(chain instanceof AndEffect andChain){
+                // Merge other AndChain instances
+                effects.addAll(andChain.effects);
+
+                return this;
+            } else if(chain == NONE || chain == null){
+                // Skip NONE
+                return this;
+            }
+
+            // Otherwise add to current chain
+            effects.add(chain);
+
             return this;
         }
-        return () -> {
-            this.run();
-            chain.run();
-        };
     }
 }
