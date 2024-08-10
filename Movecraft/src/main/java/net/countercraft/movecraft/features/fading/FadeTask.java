e73e58f587c2a91eed0417360d736f2298530679
ohnoey <cccm5dev@gmail.com>
2024-08-10 14:50:54 -0700

    load chunks

--- Movecraft/src/main/java/net/countercraft/movecraft/features/fading/FadeTask.java
+++ Movecraft/src/main/java/net/countercraft/movecraft/features/fading/FadeTask.java
@@ -2,11 +2,15 @@ package net.countercraft.movecraft.features.fading;
 
 import net.countercraft.movecraft.MovecraftLocation;
 import net.countercraft.movecraft.processing.MovecraftWorld;
+import net.countercraft.movecraft.processing.WorldManager;
 import net.countercraft.movecraft.processing.effects.Effect;
 import net.countercraft.movecraft.processing.effects.SetBlockEffect;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.block.data.BlockData;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
 import java.util.function.Supplier;
 
 /**
@@ -29,8 +33,10 @@ public class FadeTask implements Supplier<Effect> {
     public Effect get() {
         var testData = world.getData(location);
 
-        return testData.equals(compareData)
-            ? new SetBlockEffect(world, location, setData)
-            : null;
+        return () -> Objects.requireNonNull(Bukkit.getWorld(world.getWorldUUID()))
+            .getChunkAtAsync(location.toBukkit(null))
+            .thenRunAsync(() -> WorldManager.INSTANCE.submit(() -> testData.equals(compareData)
+                ? new SetBlockEffect(world, location, setData)
+                : null));
     }
 }
