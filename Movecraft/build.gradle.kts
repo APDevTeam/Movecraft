9996383aae71a255a18224850f9a97c5d32761f8
ohnoey <cccm5dev@gmail.com>
2024-08-11 07:56:45 -0700

    Add gradle tasks to run various server versions

--- Movecraft/build.gradle.kts
+++ Movecraft/build.gradle.kts
@@ -1,11 +1,20 @@
+import xyz.jpenilla.runpaper.task.RunServer
+
 plugins {
     `maven-publish`
     id("buildlogic.java-conventions")
     id("io.github.goooler.shadow") version "8.1.7"
     id("io.papermc.hangar-publish-plugin") version "0.1.2"
+    id("xyz.jpenilla.run-paper") version "2.3.0"
 }
 
 java.toolchain.languageVersion = JavaLanguageVersion.of(17)
+
+val supportedMinecraftVersions = listOf(
+    "1.18.2",
+    "1.20.6",
+    "1.21"
+)
 
 dependencies {
     runtimeOnly(project(":movecraft-v1_18", "reobf"))
@@ -34,6 +43,23 @@ tasks.processResources {
     dependsOn(project(":movecraft-datapack").tasks.build)
     filesMatching("*.yml") {
         expand(mapOf("projectVersion" to project.version))
+    }
+}
+tasks {
+    supportedMinecraftVersions.forEach { serverVersion ->
+        register<RunServer>("run-$serverVersion") {
+            minecraftVersion(serverVersion)
+            jvmArgs("-DPaper.IgnoreJavaVersion=true", "-Dcom.mojang.eula.agree=true")
+            group = "run paper"
+            runDirectory.set(file("run-$serverVersion"))
+            downloadPlugins {
+                url("https://dev.bukkit.org/projects/worldedit/files/5564367/download")
+                url("https://ci.lucko.me/job/spark/441/artifact/spark-bukkit/build/libs/spark-1.10.95-bukkit.jar")
+                hangar("ViaVersion", "5.0.3")
+                hangar("ViaBackwards", "5.0.3")
+            }
+            pluginJars(project.tasks.shadowJar.map { it.archiveFile }.get())
+        }
     }
 }
 
@@ -68,7 +94,7 @@ hangarPublish {
         platforms {
             register(io.papermc.hangarpublishplugin.model.Platforms.PAPER) {
                 jar.set(tasks.shadowJar.flatMap { it.archiveFile })
-                platformVersions.set(listOf("1.18.2", "1.20.6", "1.21"))
+                platformVersions.set(supportedMinecraftVersions)
             }
         }
     }
