b0b6a173082d6c532a97169c79444dccd6c0f68b
goodroach <hughishikawa@gmail.com>
2024-08-13 18:00:46 -1000

    Add allowInternalCollisionExplosion feature

--- api/src/main/java/net/countercraft/movecraft/craft/type/CraftType.java
+++ api/src/main/java/net/countercraft/movecraft/craft/type/CraftType.java
@@ -190,6 +190,8 @@ final public class CraftType {
     public static final NamespacedKey CRUISE_ON_PILOT_LIFETIME = buildKey("cruise_on_pilot_lifetime");
 
     public static final NamespacedKey EXPLOSION_ARMING_TIME = buildKey("explosion_arming_time");
+
+    public static final NamespacedKey ALLOW_INTERNAL_COLLISION_EXPLOSION = buildKey("allow_internal_collision_explosion");
     //endregion
 
     @Contract("_ -> new")
@@ -566,6 +568,7 @@ final public class CraftType {
         registerProperty(new BooleanProperty("mergePistonExtensions", MERGE_PISTON_EXTENSIONS, type -> false));
         registerProperty(new IntegerProperty("cruiseOnPilotLifetime", CRUISE_ON_PILOT_LIFETIME, type -> 15*20));
         registerProperty(new IntegerProperty("explosionArmingTime", EXPLOSION_ARMING_TIME, type -> 1000));
+        registerProperty(new BooleanProperty("allowInternalCollisionExplosion", ALLOW_INTERNAL_COLLISION_EXPLOSION, type -> false));
 
         /* Craft type transforms */
         // Convert speed to TICK_COOLDOWN
