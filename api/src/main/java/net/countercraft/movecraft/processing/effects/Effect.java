9ddedc75a16e126d0af7241231ea2935fe8ccba6
TylerS1066 <TylerS1066@users.noreply.github.com>
2024-08-20 19:12:46 -0500

    Use processing for wrecks (again) (#699)
    * Use processing for wrecks
    
    * Don't use Tick
    
    * Parallelize on chunks
    
    * Clamp y axis to 0 to avoid batching on sub-chunk
    
    * Fix axis modifier and add annotations
    
    * Add doc comments
    
    * Add more efficient NONE::andThen imp
    
    * Fix bugs
    
    * Update total duration to match previous and use floor
    
    * seconds to tick conversion
    
    * Fold repeated AndThen calls
    
    * load chunks
    
    * Fix 1.18.2 async chunk deadlock
    
    ---------
    
    Co-authored-by: ohnoey <cccm5dev@gmail.com>

--- api/src/main/java/net/countercraft/movecraft/processing/effects/Effect.java
+++ api/src/main/java/net/countercraft/movecraft/processing/effects/Effect.java
@@ -1,24 +1,79 @@
 package net.countercraft.movecraft.processing.effects;
 
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @FunctionalInterface
 public interface Effect {
+    /**
+     * A no-op effect for use in systems where a non-null effect is needed
+     */
+    Effect NONE = new Effect() {
+        @Override
+        public void run() {
+            // No-op
+        }
+
+        @Override
+        public boolean isAsync() {
+            return true;
+        }
+
+        @Override
+        public @NotNull Effect andThen(@Nullable Effect chain){
+            return chain == null ? this : chain;
+        }
+    };
+
     void run();
 
     default boolean isAsync(){
         return false;
     }
 
-    default @NotNull
-    Effect andThen(@Nullable Effect chain){
-        if(chain == null){
+    default @NotNull Effect andThen(@Nullable Effect chain){
+        return new AndEffect(this, chain);
+    }
+
+    class AndEffect implements Effect {
+        private final List<Effect> effects = new ArrayList<>();
+
+        public AndEffect(Effect... effects){
+            for (Effect effect : effects) {
+                andThen(effect);
+            }
+        }
+
+        @Override
+        public void run() {
+            effects.forEach(Effect::run);
+        }
+
+        @Override
+        public @NotNull Effect andThen(@Nullable Effect chain) {
+            if(this == chain){
+                // copy if chaining to self to prevent concurrent modification
+                effects.addAll(effects.stream().toList());
+
+                return this;
+            } else if(chain instanceof AndEffect andChain){
+                // Merge other AndChain instances
+                effects.addAll(andChain.effects);
+
+                return this;
+            } else if(chain == NONE || chain == null){
+                // Skip NONE
+                return this;
+            }
+
+            // Otherwise add to current chain
+            effects.add(chain);
+
             return this;
         }
-        return () -> {
-            this.run();
-            chain.run();
-        };
     }
 }
