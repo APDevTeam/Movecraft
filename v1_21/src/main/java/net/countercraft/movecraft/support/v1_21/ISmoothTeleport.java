ceb10e12fe276eff9f6f1a525f2278249c0883d9
TylerS1066 <TylerS1066@users.noreply.github.com>
2024-08-25 12:19:39 -0500

    Update 1.21 implementation

--- v1_21/src/main/java/net/countercraft/movecraft/support/v1_21/ISmoothTeleport.java
+++ v1_21/src/main/java/net/countercraft/movecraft/support/v1_21/ISmoothTeleport.java
@@ -2,21 +2,22 @@ package net.countercraft.movecraft.support.v1_21;
 
 import io.papermc.paper.entity.TeleportFlag;
 import net.countercraft.movecraft.SmoothTeleport;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class ISmoothTeleport extends SmoothTeleport {
     public void teleport(@NotNull Player player, @NotNull Location location) {
-        Bukkit.getServer().getLogger().info("Teleporting " + player.getName() + " to " + location);
         player.teleport(
                 location,
                 TeleportFlag.Relative.X,
                 TeleportFlag.Relative.Y,
                 TeleportFlag.Relative.Z,
                 TeleportFlag.Relative.PITCH,
-                TeleportFlag.Relative.YAW
+                TeleportFlag.Relative.YAW,
+                TeleportFlag.EntityState.RETAIN_OPEN_INVENTORY,
+                TeleportFlag.EntityState.RETAIN_VEHICLE,
+                TeleportFlag.EntityState.RETAIN_PASSENGERS
         );
     }
 }
