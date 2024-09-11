c09acb72a91991c2cfa248ac051c292d04bd73d2
TylerS1066 <TylerS1066@users.noreply.github.com>
2024-09-10 20:49:17 -0500

    Update SupportUtils.java

--- api/src/main/java/net/countercraft/movecraft/util/SupportUtils.java
+++ api/src/main/java/net/countercraft/movecraft/util/SupportUtils.java
@@ -4,23 +4,28 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Directional;
 import org.bukkit.block.data.FaceAttachable;
-import org.bukkit.block.data.Hangable;
+import org.bukkit.block.data.type.Lantern;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class SupportUtils {
     @Nullable
     public static BlockFace getSupportFace(@NotNull BlockData data) {
-        return switch (data) {
-            case Directional directional -> directional.getFacing().getOppositeFace();
-            case Hangable hangable when hangable.isHanging() -> BlockFace.UP;
-            case Hangable hangable when !hangable.isHanging() -> BlockFace.DOWN;
-            case FaceAttachable faceAttachable -> switch (faceAttachable.getAttachedFace()) {
-                case FLOOR -> BlockFace.DOWN;
-                case CEILING -> BlockFace.UP;
-                case WALL -> null; // Wall attachable should be Directional
-            };
-            default -> null;
-        };
+        if (data instanceof Lantern lantern)
+            return lantern.isHanging() ? BlockFace.UP : BlockFace.DOWN;
+
+        if (data instanceof Directional directional) {
+            BlockFace normalCase = directional.getFacing().getOppositeFace();
+
+            if (data instanceof FaceAttachable faceAttachable) {
+                return switch (faceAttachable.getAttachedFace()) {
+                    case FLOOR -> BlockFace.DOWN;
+                    case WALL -> normalCase;
+                    case CEILING -> BlockFace.UP;
+                };
+            }
+            return normalCase;
+        }
+        return null;
     }
 }
