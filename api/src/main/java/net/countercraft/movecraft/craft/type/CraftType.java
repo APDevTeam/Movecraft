607ac9beb834c4c7a901e50e7665e1e3d5e12c1f
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-11-17 17:07:25 +0100

    materialset, not requiredblockproperty!

--- api/src/main/java/net/countercraft/movecraft/craft/type/CraftType.java
+++ api/src/main/java/net/countercraft/movecraft/craft/type/CraftType.java
@@ -575,7 +575,7 @@ final public class CraftType {
         registerProperty(new IntegerProperty("explosionArmingTime", EXPLOSION_ARMING_TIME, type -> 1000));
         registerProperty(new BooleanProperty("allowInternalCollisionExplosion", ALLOW_INTERNAL_COLLISION_EXPLOSION, type -> false));
 
-        registerProperty(new RequiredBlockProperty("moveBreakBlocks", MOVE_BREAK_BLOCKS, type -> new HashSet<>()));
+        registerProperty(new MaterialSetProperty("moveBreakBlocks", MOVE_BREAK_BLOCKS));
 
         /* Craft type transforms */
         // Convert speed to TICK_COOLDOWN
