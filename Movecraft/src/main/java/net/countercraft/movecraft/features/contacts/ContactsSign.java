a1722f48072c5c7dff2ca9c5d3784edc7aedf207
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-11-23 11:02:26 +0100

    use name if possible

--- Movecraft/src/main/java/net/countercraft/movecraft/features/contacts/ContactsSign.java
+++ Movecraft/src/main/java/net/countercraft/movecraft/features/contacts/ContactsSign.java
@@ -35,6 +35,9 @@ public class ContactsSign extends AbstractInformationSign {
         int distanceSquared = baseCenter.distanceSquared(targetCenter);
 
         String craftTypeName = target.getType().getStringProperty(CraftType.NAME);
+        if (target.getName() != null && !target.getName().isBlank()) {
+            craftTypeName = target.getName();
+        }
         if (craftTypeName.length() > 9)
             craftTypeName = craftTypeName.substring(0, 7);
 
