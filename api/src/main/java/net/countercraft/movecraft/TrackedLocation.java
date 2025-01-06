2e4d56a5f302d6f997ded50e8fb32a65b25c4f5a
goodroach <hughishikawa@gmail.com>
2025-01-05 16:07:25 -1000

    improve TrackedLocation

--- api/src/main/java/net/countercraft/movecraft/TrackedLocation.java
+++ api/src/main/java/net/countercraft/movecraft/TrackedLocation.java
@@ -2,7 +2,10 @@ package net.countercraft.movecraft;
 
 import net.countercraft.movecraft.craft.Craft;
 import net.countercraft.movecraft.util.MathUtils;
+import org.bukkit.NamespacedKey;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
 
 public class TrackedLocation {
     private MovecraftLocation offSet;
@@ -18,6 +21,22 @@ public class TrackedLocation {
         this.craft = craft;
         MovecraftLocation midPoint = craft.getHitBox().getMidPoint();
         offSet = location.subtract(midPoint);
+    }
+
+    /**
+     * Creates a new TrackedLocation instance which tracks a location about a craft's midpoint.
+     * This additionally automatically adds the tracked location to the craft's list.
+     * @param craft The craft that's that tied to the location.
+     * @param location The absolute position to track. This location will be stored as a relative
+     *                 location to the craft's central hitbox.
+     * @param key The namespaced key that stores a set of tracked location within the craft.
+     */
+    public TrackedLocation(@NotNull Craft craft, @NotNull MovecraftLocation location, NamespacedKey key) {
+        this(craft, location);
+        if (craft.getTrackedLocations().get(key) == null) {
+            craft.getTrackedLocations().put(key, new HashSet<>());
+        }
+        craft.getTrackedLocations().get(key).add(this);
     }
 
     /**
@@ -43,5 +62,13 @@ public class TrackedLocation {
      */
     public MovecraftLocation getOffSet() {
         return offSet;
+    }
+
+    /**
+     * Gets the tracked location's associated craft.
+     * @return Returns the craft associated with the location.
+     */
+    public Craft getCraft() {
+        return craft;
     }
 }
