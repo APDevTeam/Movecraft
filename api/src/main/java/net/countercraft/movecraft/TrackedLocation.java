8d312f1693602c361a63629d9e8a0541e18db679
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-01-18 18:49:12 +0100

    clean up
    (cherry picked from commit 41514c60508007431ce48dbe00da50f9f093d96a)

--- api/src/main/java/net/countercraft/movecraft/TrackedLocation.java
+++ api/src/main/java/net/countercraft/movecraft/TrackedLocation.java
@@ -39,23 +39,11 @@ public class TrackedLocation {
      * @param rotation A clockwise or counter-clockwise direction to rotate.
      */
     public void rotate(MovecraftRotation rotation, MovecraftLocation origin) {
-        // TODO: SOMEHOW "absolute" and "origin" can end up to be the same thing?! WTF?!
         MovecraftLocation absolute = this.getAbsoluteLocation();
-        System.out.println("Absolute: " + absolute.toString());
         MovecraftLocation vector = MathUtils.rotateVec(rotation, absolute.subtract(origin));
-        //MovecraftLocation vector = absolute.subtract(origin);
-        //int x = rotation == MovecraftRotation.ANTICLOCKWISE ? vector.getZ() : -vector.getZ();
-        //int z = rotation == MovecraftRotation.ANTICLOCKWISE ? -vector.getX() : vector.getX();
-        //vector = new MovecraftLocation(x, vector.getY(), z);
 
         MovecraftLocation newAbsolute = origin.add(vector);
-        System.out.println("New absolute: " + newAbsolute.toString());
-        // Ugly hack, but necessary
-        //MovecraftLocation craftMidPoint = this.craft.getHitBox().getMidPoint();
-        //this.offSet = newAbsolute.subtract(craftMidPoint);
         reinit(newAbsolute);
-
-        System.out.println("New absolute after recalculating: " + this.getAbsoluteLocation().toString());
     }
 
     public void translate(final int dx, final int dy, final int dz) {
@@ -77,25 +65,12 @@ public class TrackedLocation {
      * @param craft
      * @param location
      */
+    @Deprecated(forRemoval = true)
     public void reset(@NotNull Craft craft, @NotNull MovecraftLocation location) {
-        /*if (craft == this.craft) {
-            return;
-        }
-        if (this.craft != null) {
-            if (!(
-                    // From parent to subcraft
-                    (craft instanceof SubCraft subCraft && subCraft.getParent() == this.craft)
-                    // From subcraft back to parent
-                    || (this.craft instanceof SubCraft subCraft2 && subCraft2.getParent() == craft)
-                )) {
-                throw new IllegalStateException("Only ever call this when transferring from or to subcraft!");
-            }
-        }
-        this.craft = craft;
-        MovecraftLocation midPoint = craft.getHitBox().getMidPoint();
-        System.out.println("Location: " + location.toString());
-        System.out.println("Midpoint: " + midPoint.toString());
-        offSet = location.subtract(midPoint);*/
+        reset(location);
+    }
+
+    public void reset(@NotNull MovecraftLocation location) {
         reinit(location);
     }
 
