8fe3979777300c632b2ced57aae1de6e490ee22e
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-01-18 18:46:32 +0100

    make trackedlocations a bit more stupid
    it does not need to know the craft.
    Also just moving around the cached coordinates is more reliable as it does not rely on a changing center of the craft

--- api/src/main/java/net/countercraft/movecraft/TrackedLocation.java
+++ api/src/main/java/net/countercraft/movecraft/TrackedLocation.java
@@ -1,22 +1,37 @@
 package net.countercraft.movecraft;
 
 import net.countercraft.movecraft.craft.Craft;
-import net.countercraft.movecraft.craft.SubCraft;
 import net.countercraft.movecraft.util.MathUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class TrackedLocation {
-    private MovecraftLocation offSet;
-    private Craft craft;
+
+    private int x;
+    private int y;
+    private int z;
 
     /**
      * Creates a new TrackedLocation instance which tracks a location about a craft's midpoint.
-     * @param craft The craft that's that tied to the location.
      * @param location The absolute position to track. This location will be stored as a relative
      *                 location to the craft's central hitbox.
      */
+    public TrackedLocation(@NotNull MovecraftLocation location) {
+        reinit(location);
+    }
+
+    @Deprecated(forRemoval = true)
     public TrackedLocation(@NotNull Craft craft, @NotNull MovecraftLocation location) {
-        this.reset(craft, location);
+        this(location);
+    }
+
+    protected void reinit(@NotNull MovecraftLocation location) {
+        reinit(location.getX(), location.getY(), location.getZ());
+    }
+
+    protected void reinit(final int x, final int y, final int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     /**
@@ -28,13 +43,25 @@ public class TrackedLocation {
         MovecraftLocation absolute = this.getAbsoluteLocation();
         System.out.println("Absolute: " + absolute.toString());
         MovecraftLocation vector = MathUtils.rotateVec(rotation, absolute.subtract(origin));
+        //MovecraftLocation vector = absolute.subtract(origin);
+        //int x = rotation == MovecraftRotation.ANTICLOCKWISE ? vector.getZ() : -vector.getZ();
+        //int z = rotation == MovecraftRotation.ANTICLOCKWISE ? -vector.getX() : vector.getX();
+        //vector = new MovecraftLocation(x, vector.getY(), z);
 
         MovecraftLocation newAbsolute = origin.add(vector);
         System.out.println("New absolute: " + newAbsolute.toString());
         // Ugly hack, but necessary
-        MovecraftLocation craftMidPoint = this.craft.getHitBox().getMidPoint();
-        this.offSet = newAbsolute.subtract(craftMidPoint);
+        //MovecraftLocation craftMidPoint = this.craft.getHitBox().getMidPoint();
+        //this.offSet = newAbsolute.subtract(craftMidPoint);
+        reinit(newAbsolute);
+
         System.out.println("New absolute after recalculating: " + this.getAbsoluteLocation().toString());
+    }
+
+    public void translate(final int dx, final int dy, final int dz) {
+        this.x += dx;
+        this.y += dy;
+        this.z += dz;
     }
 
     /**
@@ -42,16 +69,7 @@ public class TrackedLocation {
      * @return Returns the absolute location instead of a vector.
      */
     public MovecraftLocation getAbsoluteLocation() {
-        MovecraftLocation midPoint = craft.getHitBox().getMidPoint();
-        return offSet.add(midPoint);
-    }
-
-    /**
-     * Gets the stored location as a position vector relative to the midpoint.
-     * @return Returns the absolute location instead of a vector.
-     */
-    public MovecraftLocation getOffSet() {
-        return offSet;
+        return new MovecraftLocation(this.x, this. y, this.z);
     }
 
     /**
@@ -60,7 +78,7 @@ public class TrackedLocation {
      * @param location
      */
     public void reset(@NotNull Craft craft, @NotNull MovecraftLocation location) {
-        if (craft == this.craft) {
+        /*if (craft == this.craft) {
             return;
         }
         if (this.craft != null) {
@@ -77,11 +95,8 @@ public class TrackedLocation {
         MovecraftLocation midPoint = craft.getHitBox().getMidPoint();
         System.out.println("Location: " + location.toString());
         System.out.println("Midpoint: " + midPoint.toString());
-        offSet = location.subtract(midPoint);
-    }
-
-    public void reset(@NotNull MovecraftLocation location) {
-        this.reset(this.craft, location);
+        offSet = location.subtract(midPoint);*/
+        reinit(location);
     }
 
 }
