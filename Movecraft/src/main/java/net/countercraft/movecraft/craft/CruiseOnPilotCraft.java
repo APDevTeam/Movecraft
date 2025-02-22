7bda75b330b6eb50bd4c4acfb61868fde46c2aea
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-02-22 13:04:40 +0100

    fix message spam from torps and bombs

--- Movecraft/src/main/java/net/countercraft/movecraft/craft/CruiseOnPilotCraft.java
+++ Movecraft/src/main/java/net/countercraft/movecraft/craft/CruiseOnPilotCraft.java
@@ -1,6 +1,7 @@
 package net.countercraft.movecraft.craft;
 
 import net.countercraft.movecraft.craft.type.CraftType;
+import net.kyori.adventure.audience.Audience;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -19,19 +20,19 @@ public class CruiseOnPilotCraft extends BaseCraft implements PilotedCraft {
         this.pilot = new WeakReference<>(pilot);
         // Copy UUID just to be safe
         this.pilotUUID = UUID.fromString(pilot.getUniqueId().toString());
+        this.setAudience(Audience.empty());
     }
 
     @Nullable
     @Override
     public Player getPilot() {
+        // Do not re-set the audience here! Crafts like this are like torpedoes, we dont need to receive messages from it
         if (this.pilot.get() == null) {
             this.pilot = new WeakReference<> (Bukkit.getPlayer(this.getPilotUUID()));
-            this.setAudience(this.pilot.get());
         } else {
             Player bukkitPilot = Bukkit.getPlayer(this.getPilotUUID());
             if (!this.pilot.get().isOnline() || (this.pilot.get() != bukkitPilot)) {
                 this.pilot = new WeakReference<> (bukkitPilot);
-                this.setAudience(this.pilot.get());
             }
         }
         return this.pilot.get();
