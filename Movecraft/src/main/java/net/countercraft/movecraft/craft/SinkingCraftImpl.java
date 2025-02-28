7dc3685e487e75828829438b8bc1161554bb126c
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-02-28 18:22:50 +0100

    implement custom contactProvider for sinking crafts => NO sinking contacts

--- Movecraft/src/main/java/net/countercraft/movecraft/craft/SinkingCraftImpl.java
+++ Movecraft/src/main/java/net/countercraft/movecraft/craft/SinkingCraftImpl.java
@@ -1,9 +1,13 @@
 package net.countercraft.movecraft.craft;
 
+import net.countercraft.movecraft.MovecraftLocation;
+import net.countercraft.movecraft.features.contacts.ContactProvider;
+import net.countercraft.movecraft.processing.MovecraftWorld;
 import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
 import org.jetbrains.annotations.NotNull;
 
-public class SinkingCraftImpl extends BaseCraft implements SinkingCraft {
+public class SinkingCraftImpl extends BaseCraft implements SinkingCraft, ContactProvider {
     public SinkingCraftImpl(@NotNull Craft original) {
         super(original.getType(), original.getWorld());
         hitBox = original.getHitBox();
@@ -19,5 +23,25 @@ public class SinkingCraftImpl extends BaseCraft implements SinkingCraft {
     @Override
     public @NotNull Audience getAudience() {
         return Audience.empty();
+    }
+
+    @Override
+    public Component getDetectedMessage(boolean isNew, Craft detectingCraft) {
+        return Component.empty();
+    }
+
+    @Override
+    public boolean contactPickedUpBy(Craft other) {
+        return false;
+    }
+
+    @Override
+    public MovecraftLocation getContactLocation() {
+        return this.getCraftOrigin();
+    }
+
+    @Override
+    public double getDetectionMultiplier(boolean overWaterLine, MovecraftWorld world) {
+        return 0;
     }
 }
