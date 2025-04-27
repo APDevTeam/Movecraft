9feccb577494a3621e31df31238e28bb48600e1e
TylerS1066 <TylerS1066@users.noreply.github.com>
2025-04-27 12:51:42 -0500

    Update versions

--- v1_21/src/main/java/net/countercraft/movecraft/compat/v1_21/IWorldHandler.java
+++ v1_21/src/main/java/net/countercraft/movecraft/compat/v1_21/IWorldHandler.java
@@ -52,7 +52,7 @@ public class IWorldHandler extends WorldHandler {
 
     public IWorldHandler() {
         String version = Bukkit.getServer().getMinecraftVersion();
-        if (!version.equals("1.21.4"))
+        if (!version.equals("1.21.5"))
             throw new IllegalStateException("Movecraft is not compatible with this version of Minecraft: " + version);
     }
 
