4aef3ec888a5910eeb17fe2b168b185eb29b4115
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-06-18 11:21:59 +0200

    add typedkey and tpyesaferegistry helper objects

---
+++ api/src/main/java/net/countercraft/movecraft/util/registration/TypedContainer.java
@@ -0,0 +1,41 @@
+package net.countercraft.movecraft.util.registration;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class TypedContainer<K extends TypedKey<?>> {
+
+    private final Map<K, Object> backing;
+
+    public TypedContainer(Supplier<Map<K, Object>> mapCreator) {
+        this.backing = mapCreator.get();
+    }
+
+    public TypedContainer() {
+        this.backing = new ConcurrentHashMap<>();
+    }
+
+    protected <T> @Nullable T get(@NotNull TypedKey<T> key, Function<K, T> defaultSupplier) {
+        Object stored = backing.computeIfAbsent((K) key, defaultSupplier);
+        try {
+            //noinspection unchecked
+            return (T) stored;
+        } catch (ClassCastException cce) {
+            throw new IllegalStateException(String.format("The provided key %s has an invalid value type.", key), cce);
+        }
+    }
+
+    protected <T> void set(@NotNull TypedKey<T> tagKey, @NotNull T value) {
+        backing.put((K) tagKey, value);
+    }
+
+    protected <T> boolean has(@NotNull K tagKey) {
+        return backing.containsKey(tagKey);
+    }
+
+}
