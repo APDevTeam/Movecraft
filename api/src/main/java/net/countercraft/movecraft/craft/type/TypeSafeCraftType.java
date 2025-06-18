23fa26388b2da1d3e4941bdc0de232d846eb50a9
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-06-18 11:24:26 +0200

    begin implementing TypeSafeCraftType

---
+++ api/src/main/java/net/countercraft/movecraft/craft/type/TypeSafeCraftType.java
@@ -0,0 +1,95 @@
+package net.countercraft.movecraft.craft.type;
+
+import net.countercraft.movecraft.util.registration.SimpleRegistry;
+import net.countercraft.movecraft.util.registration.TypedContainer;
+import net.countercraft.movecraft.util.registration.TypedKey;
+import org.bukkit.NamespacedKey;
+import org.jetbrains.annotations.NotNull;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TypeSafeCraftType extends TypedContainer<PropertyKey<?>> {
+
+    static final SimpleRegistry<NamespacedKey, PropertyKey> PROPERTY_REGISTRY = new SimpleRegistry();
+
+    @NotNull
+    public static TypeSafeCraftType load(@NotNull File file) {
+        final InputStream input;
+        try {
+            input = new FileInputStream(file);
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return new TypeSafeCraftType();
+        }
+        try(input) {
+            Yaml yaml = new Yaml();
+            return buildType(yaml.load(input));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return new TypeSafeCraftType();
+        }
+    }
+
+    private TypeSafeCraftType() {
+        super();
+    }
+
+    private static TypeSafeCraftType buildType(Map<String, Object> yamlMapping) {
+        TypeSafeCraftType result = new TypeSafeCraftType();
+
+        // Structure: Simple map per namespace inside the file
+        // Helps to differentiate things
+
+        Map<String, Map<String, Object>> namespaces = new HashMap<>();
+        for (Map.Entry<String, Object> entry : yamlMapping.entrySet()) {
+            String namespace = "movecraft";
+            if (entry.getValue() instanceof Map) {
+                namespace = entry.getKey();
+                try {
+                    namespaces.put(namespace, (Map<String, Object>) entry.getValue());
+                } catch(ClassCastException cce) {
+                    // TODO: log error
+                }
+            } else {
+                namespaces.putIfAbsent(namespace, yamlMapping);
+            }
+        }
+        for (Map.Entry<String, Map<String, Object>> entry : namespaces.entrySet()) {
+            readNamespace(entry.getKey(), entry.getValue(), result);
+        }
+
+        return result;
+    }
+
+    private static <T> void readNamespace(final String namespace, final Map<String, Object> data, final TypeSafeCraftType type) {
+        for (Map.Entry<String, Object> entry : data.entrySet()) {
+            NamespacedKey key = NamespacedKey.fromString(entry.getKey());
+            PropertyKey<T> propertyKey = PROPERTY_REGISTRY.get(key);
+            if (propertyKey == null) {
+                continue;
+            }
+
+            try {
+                type.set(propertyKey, propertyKey.read(entry.getValue(), type));
+            } catch(ClassCastException cce) {
+                // TODO: Log error
+            }
+        }
+    }
+
+    @Override
+    protected <T> void set(@NotNull TypedKey<T> tagKey, @NotNull T value) {
+        super.set(tagKey, value);
+    }
+
+    public CraftProperties createCraftProperties() {
+        // TODO: Implement
+        return null;
+    }
+
+}
