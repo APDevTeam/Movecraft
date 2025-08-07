492d95c3ce3af7e9302c389920d15a2320c5089e
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-08-07 08:59:46 +0200

    Move name logic to static void

--- Movecraft/src/main/java/net/countercraft/movecraft/sign/NameSign.java
+++ Movecraft/src/main/java/net/countercraft/movecraft/sign/NameSign.java
@@ -65,6 +65,10 @@ public class NameSign extends AbstractCraftSign {
     @Override
     public void onCraftDetect(CraftDetectEvent event, SignListener.SignWrapper sign) {
         Craft craft = event.getCraft();
+        tryApplyName(craft, sign);
+    }
+
+    public static void tryApplyName(final Craft craft, final SignListener.SignWrapper sign) {
         if (craft != null && craft instanceof PilotedCraft pc) {
             if (pc.getPilot() == null) {
                 return;
