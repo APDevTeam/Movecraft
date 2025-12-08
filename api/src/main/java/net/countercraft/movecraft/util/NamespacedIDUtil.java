2a22e7aa9759f2734d24b326a9972c8545c61261
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-12-08 19:27:26 +0100

    add utility to retrieve NamespacedKeys

--- api/src/main/java/net/countercraft/movecraft/util/NamespacedIDUtil.java
+++ api/src/main/java/net/countercraft/movecraft/util/NamespacedIDUtil.java
@@ -1,2 +1,23 @@
-package net.countercraft.movecraft.util;public class NamespacedIDUtil {
+package net.countercraft.movecraft.util;
+
+import org.bukkit.Keyed;
+import org.bukkit.NamespacedKey;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+
+public class NamespacedIDUtil {
+
+    public static NamespacedKey getBlockID(final Block block) {
+        BlockData blockData = block.getBlockData();
+        return getBlockID(blockData);
+    }
+
+    public static NamespacedKey getBlockID(final BlockData blockData) {
+        if (blockData == null || !(blockData instanceof Keyed)) {
+            return blockData.getMaterial().getKey();
+        } else {
+            Keyed keyed = (Keyed) blockData;
+            return keyed.getKey();
+        }
+    }
 }
