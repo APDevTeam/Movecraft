e3183135c2a54d78dc3d780de19848d055b13998
TylerS1066 <TylerS1066@users.noreply.github.com>
2025-12-13 18:10:08 -0600

    Add 1.21.11

--- Movecraft/build.gradle.kts
+++ Movecraft/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     runtimeOnly(project(":movecraft-v1_21_5", "reobf"))
     runtimeOnly(project(":movecraft-v1_21_8", "reobf"))
     runtimeOnly(project(":movecraft-v1_21_10", "reobf"))
+    runtimeOnly(project(":movecraft-v1_21_11", "reobf"))
     implementation(project(":movecraft-api"))
     compileOnly("org.yaml:snakeyaml:2.0")
 }
@@ -31,6 +32,7 @@ tasks.shadowJar {
         include(project(":movecraft-v1_21_5"))
         include(project(":movecraft-v1_21_8"))
         include(project(":movecraft-v1_21_10"))
+        include(project(":movecraft-v1_21_11"))
     }
 }
 
@@ -74,7 +76,7 @@ hangarPublish {
         platforms {
             register(io.papermc.hangarpublishplugin.model.Platforms.PAPER) {
                 jar.set(tasks.shadowJar.flatMap { it.archiveFile })
-                platformVersions.set(listOf("1.20.6", "1.21.1", "1.21.4", "1.21.5", "1.21.8", "1.21.10"))
+                platformVersions.set(listOf("1.20.6", "1.21.1", "1.21.4", "1.21.5", "1.21.8", "1.21.10", "1.21.11"))
             }
         }
     }
