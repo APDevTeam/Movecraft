490fb79dc18e720062fea8c564393b3f1541a135
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-12-19 19:57:51 +0100

    migrate translation validators

--- Movecraft/src/main/java/net/countercraft/movecraft/processing/tasks/translation/validators/HoverValidator.java
+++ Movecraft/src/main/java/net/countercraft/movecraft/processing/tasks/translation/validators/HoverValidator.java
@@ -1,29 +1,31 @@
 package net.countercraft.movecraft.processing.tasks.translation.validators;
 
 import net.countercraft.movecraft.MovecraftLocation;
-import net.countercraft.movecraft.craft.type.CraftType;
+import net.countercraft.movecraft.craft.type.PropertyKeys;
+import net.countercraft.movecraft.craft.type.TypeSafeCraftType;
 import net.countercraft.movecraft.localisation.I18nSupport;
 import net.countercraft.movecraft.processing.MovecraftWorld;
 import net.countercraft.movecraft.processing.functions.Result;
 import net.countercraft.movecraft.processing.functions.TetradicPredicate;
+import net.countercraft.movecraft.util.NamespacedIDUtil;
 import net.countercraft.movecraft.util.hitboxes.HitBox;
-import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-public class HoverValidator implements TetradicPredicate<MovecraftLocation, MovecraftWorld, HitBox, CraftType> {
+public class HoverValidator implements TetradicPredicate<MovecraftLocation, MovecraftWorld, HitBox, TypeSafeCraftType> {
     @Override
     @Contract(pure = true)
-    public @NotNull Result validate(@NotNull MovecraftLocation translation, @NotNull MovecraftWorld world, @NotNull HitBox hitBox, @NotNull CraftType type) {
-        if (type.getMaterialSetProperty(CraftType.FORBIDDEN_HOVER_OVER_BLOCKS).size() > 0){
+    public @NotNull Result validate(@NotNull MovecraftLocation translation, @NotNull MovecraftWorld world, @NotNull HitBox hitBox, @NotNull TypeSafeCraftType type) {
+        if (type.get(PropertyKeys.FORBIDDEN_HOVER_OVER_BLOCKS).size() > 0){
             MovecraftLocation test = new MovecraftLocation(hitBox.getMidPoint().getX(), hitBox.getMinY(), hitBox.getMidPoint().getZ());
             test = test.translate(0, -1, 0);
             while (world.getMaterial(test).isAir()) {
                 test = test.translate(0, -1, 0);
             }
-            Material testType = world.getMaterial(test);
-            if (type.getMaterialSetProperty(CraftType.FORBIDDEN_HOVER_OVER_BLOCKS).contains(testType)){
-                return Result.failWithMessage(String.format(I18nSupport.getInternationalisedString("Translation - Failed Craft over block"), testType.name().toLowerCase().replace("_", " ")));
+            NamespacedKey testType = NamespacedIDUtil.getBlockID(world.getData(test));
+            if (type.get(PropertyKeys.FORBIDDEN_HOVER_OVER_BLOCKS).contains(testType)){
+                return Result.failWithMessage(String.format(I18nSupport.getInternationalisedString("Translation - Failed Craft over block"), testType.toString().replace("_", " ")));
             }
         }
         return Result.succeed();
--- Movecraft/src/main/java/net/countercraft/movecraft/processing/tasks/translation/validators/MaxHeightValidator.java
+++ Movecraft/src/main/java/net/countercraft/movecraft/processing/tasks/translation/validators/MaxHeightValidator.java
@@ -1,7 +1,8 @@
 package net.countercraft.movecraft.processing.tasks.translation.validators;
 
 import net.countercraft.movecraft.MovecraftLocation;
-import net.countercraft.movecraft.craft.type.CraftType;
+import net.countercraft.movecraft.craft.type.PropertyKeys;
+import net.countercraft.movecraft.craft.type.TypeSafeCraftType;
 import net.countercraft.movecraft.localisation.I18nSupport;
 import net.countercraft.movecraft.processing.MovecraftWorld;
 import net.countercraft.movecraft.processing.functions.Result;
@@ -10,12 +11,12 @@ import net.countercraft.movecraft.util.hitboxes.HitBox;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-public class MaxHeightValidator implements TetradicPredicate<MovecraftLocation, MovecraftWorld, HitBox, CraftType> {
+public class MaxHeightValidator implements TetradicPredicate<MovecraftLocation, MovecraftWorld, HitBox, TypeSafeCraftType> {
     @Override
     @Contract(pure = true)
-    public @NotNull Result validate(@NotNull MovecraftLocation translation, @NotNull MovecraftWorld world, @NotNull HitBox hitBox, @NotNull CraftType type){
-        int maxHeightLimit = (int) type.getPerWorldProperty(CraftType.PER_WORLD_MAX_HEIGHT_LIMIT, world);
-        if (translation.getY() > 0 && hitBox.getMaxY() > maxHeightLimit && type.getFloatProperty(CraftType.COLLISION_EXPLOSION) <= 0F) {
+    public @NotNull Result validate(@NotNull MovecraftLocation translation, @NotNull MovecraftWorld world, @NotNull HitBox hitBox, @NotNull TypeSafeCraftType type){
+        int maxHeightLimit = type.get(PropertyKeys.MAX_HEIGHT_LIMIT, world);
+        if (translation.getY() > 0 && hitBox.getMaxY() > maxHeightLimit && type.get(PropertyKeys.COLLISION_EXPLOSION) <= 0F) {
             return Result.failWithMessage(I18nSupport.getInternationalisedString("Translation - Failed Craft hit height limit"));
         }
         return Result.succeed();
