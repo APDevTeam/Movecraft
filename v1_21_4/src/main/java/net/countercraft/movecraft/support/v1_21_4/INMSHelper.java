c80db35aee251dbfcdbd504d7379fe46f180d7dc
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-01-01 21:17:16 +0100

    add method to directly modify burn and totalBurnTime of furnaces

--- v1_21_4/src/main/java/net/countercraft/movecraft/support/v1_21_4/INMSHelper.java
+++ v1_21_4/src/main/java/net/countercraft/movecraft/support/v1_21_4/INMSHelper.java
@@ -1,11 +1,19 @@
 package net.countercraft.movecraft.support.v1_21_4;
 
 import net.countercraft.movecraft.NMSHelper;
+import net.countercraft.movecraft.util.ReflectUtils;
+import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.level.block.entity.FuelValues;
 import org.bukkit.World;
+import org.bukkit.block.Furnace;
 import org.bukkit.craftbukkit.CraftWorld;
+import org.bukkit.craftbukkit.block.CraftBlockEntityState;
+import org.bukkit.craftbukkit.block.CraftFurnace;
 import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
 
 public class INMSHelper extends NMSHelper {
 
@@ -25,5 +33,43 @@ public class INMSHelper extends NMSHelper {
             return getFuelValues(world).burnDuration(((CraftItemStack)itemStack).handle);
         }
         return 0;
+    }
+
+    static final @NotNull Field FURNACE_LIT_TOTAL_TIME;
+    static final @NotNull Field CRAFT_BLOCK_ENTITY_STATE_SNAPSHOT;
+
+    static {
+        try {
+            FURNACE_LIT_TOTAL_TIME = ReflectUtils.getField(AbstractFurnaceBlockEntity.class, "litTotalTime");
+            CRAFT_BLOCK_ENTITY_STATE_SNAPSHOT = ReflectUtils.getField(CraftBlockEntityState.class, "snapshot");
+            CRAFT_BLOCK_ENTITY_STATE_SNAPSHOT.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void setFurnaceBurnTime(int burnTime, int totalBurnTime, Furnace furnace) {
+        Object snapshot = null;
+        try {
+            snapshot = CRAFT_BLOCK_ENTITY_STATE_SNAPSHOT.get(((CraftFurnace)furnace));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return;
+        }
+        if (snapshot == null) {
+            return;
+        }
+        if (!(snapshot instanceof AbstractFurnaceBlockEntity)) {
+            return;
+        }
+
+        AbstractFurnaceBlockEntity furnaceBlockEntity = (AbstractFurnaceBlockEntity) snapshot;
+        try {
+            FURNACE_LIT_TOTAL_TIME.set(furnaceBlockEntity, totalBurnTime);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        furnaceBlockEntity.litTimeRemaining = burnTime;
     }
 }
