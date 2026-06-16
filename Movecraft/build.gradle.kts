b49f891c85a11f69e04a886c696bda838c1bd4e9
TylerS1066 <TylerS1066@users.noreply.github.com>
2026-06-16 17:18:18 -0500

    26.2 (#772)
    * First attempt at 26.2 support
    
    * Final fixes

--- Movecraft/build.gradle.kts
+++ Movecraft/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     runtimeOnly(project(":movecraft-v1_21_10"))
     runtimeOnly(project(":movecraft-v1_21_11"))
     runtimeOnly(project(":movecraft-v26_1_2"))
+    runtimeOnly(project(":movecraft-v26_2"))
     implementation(project(":movecraft-api"))
     compileOnly("org.yaml:snakeyaml:2.0")
 }
@@ -35,6 +36,7 @@ tasks.shadowJar {
         include(project(":movecraft-v1_21_10"))
         include(project(":movecraft-v1_21_11"))
         include(project(":movecraft-v26_1_2"))
+        include(project(":movecraft-v26_2"))
     }
 
     manifest.attributes(
@@ -82,7 +84,7 @@ hangarPublish {
         platforms {
             register(io.papermc.hangarpublishplugin.model.Platforms.PAPER) {
                 jar.set(tasks.shadowJar.flatMap { it.archiveFile })
-                platformVersions.set(listOf("1.20.6", "1.21.1", "1.21.4", "1.21.5", "1.21.8", "1.21.10", "1.21.11", "26.1.2"))
+                platformVersions.set(listOf("1.20.6", "1.21.1", "1.21.4", "1.21.5", "1.21.8", "1.21.10", "1.21.11", "26.1.2", "26.2"))
             }
         }
     }
